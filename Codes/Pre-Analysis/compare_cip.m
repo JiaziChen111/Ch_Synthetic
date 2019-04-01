cb36747e63574162254c1e350a355c88a306ada3
Pavel Solís <pavel.solis@gmail.com>
2019-04-01 11:46:34 -0400

    Allows to compare more than one (rho) variable, plus options (input, save figure)

--- Codes/Pre-Analysis/compare_cip.m
+++ Codes/Pre-Analysis/compare_cip.m
@@ -1,8 +1,11 @@
 %% Compare CIP Calculations
 % This code compares my CIP variables with those of Du, Im & Schreger (2018).
+% Assumes that header_daily and dataset_daily are in the workspace and
+% contain the CIP variables (rho, spread, CIP deviations).
 %
 % Pavel Solís (pavel.solis@gmail.com), March 2019
-%%
+% 
+%% Prepare Currencies and Tenors
 if ~exist('T_cip','var')                            % Run code if T_cip is not in the workspace
     run read_cip.m
 end
@@ -18,43 +21,59 @@ tnrsnum  = strrep(tnrsnum,'y','');                  % Remove 'y' from all tenors
 tnrsnum  = cellfun(@str2num,tnrsnum);               % Convert cell array to numbers
 [tnrsnum,idx] = sort(tnrsnum);                      % Sort all tenors in ascending order
 tnrscell = tnrscell(idx);                           % Reorder cell array of tenors in ascending order
+ntnrs    = length(tnrscell);
 
-%% Compare Forward Premium for All Maturities
+%% For Each Country Compare CIP Variables for All Maturities
+
+varDIS = {'rho','diff_y','cip_govt'};
+varOWN = {'RHO','LCSPRD','CIPDEV'};
+pltname = {'Forward Premium','Spread','CIP Deviations'};
+savefig = true; tf_input = false;
+
+for j = 1:length(varDIS)
 for k = 1:length(curncs)
-    corrs = [];
-    for l = 1:length(tnrscell)
-        LC = curncs{k}; tnr = tnrscell{l};
+    close all
+    corrs = nan(ntnrs,2);
+    for l = 1:ntnrs
+        LC = curncs{k}; tnr = tnrscell{l}; corrs(l,1) = tnrsnum(l);
 
         rows  = T_cip.currency==LC & T_cip.tenor==tnr;
-        aux   = table(T_cip.date(rows),T_cip{rows,'rho'},'VariableNames',{'date',['dis' tnr]});
-        TTdis = table2timetable(aux);               % Create timetable for DIS rho
+        aux   = table(T_cip.date(rows),T_cip{rows,varDIS{j}},'VariableNames',{'date',['dis' tnr]});
+        TTdis = table2timetable(aux);               % Create timetable for DIS variable
         
         hdr  = header_daily;
-        fltr = ismember(hdr(:,1),LC) & ismember(hdr(:,2),'RHO') & ismember(hdr(:,5),num2str(tnrsnum(l)));
-        if sum(fltr) > 0                            % Proceed if tenor was calculated (i.e. data was available)
+        fltr = ismember(hdr(:,1),LC) & ismember(hdr(:,2),varOWN{j}) & ismember(hdr(:,5),num2str(tnrsnum(l)));
+        if sum(fltr) > 0                            % Compare if tenor was calculated (i.e. data was available)
             t     = datetime(dataset_daily(:,1),'ConvertFrom','datenum');
             aux   = table(t,dataset_daily(:,fltr),'VariableNames',{'date',['own' tnr]});
-            TTown = table2timetable(aux);           % Create timetable for own rho
+            TTown = table2timetable(aux);           % Create timetable for own variable
             
-            TT    = synchronize(TTdis,TTown,'intersection');   % Match values of rho in time
-            corrs = [corrs; tnrsnum(l), corr(TT.(['dis' tnr]),TT.(['own' tnr]),'Rows','complete')];
+            TT = synchronize(TTdis,TTown,'intersection'); % Match values of variables in time
+            corrs(l,2) = corr(TT.(['dis' tnr]),TT.(['own' tnr]),'Rows','complete');
         end
 
         figure;
-        plot(T_cip.date(rows),T_cip{rows,'rho'})
-        if sum(fltr) > 0                            % Proceed if tenor was calculated (i.e. data was available)
+        if strcmp(varDIS{j},'cip_govt')             % Plot CIP deviations in percentage points
+            plot(T_cip.date(rows),T_cip{rows,varDIS{j}}./100)
+        else
+            plot(T_cip.date(rows),T_cip{rows,varDIS{j}})
+        end
+        if sum(fltr) > 0                            % Plot own if tenor was calculated (i.e. data was available)
             hold on
             plot(t,dataset_daily(:,fltr))
             legend('DIS','Own')
         else
             legend('DIS')
         end
-        title([LC ': Forward Premium ' tnr])
+        title([pltname{j} ': ' LC ' ' tnr])
         ylabel('%')
         datetick('x','yy','keeplimits')
+        figname = [varDIS{j} '_' LC '_' tnr 'yr'];
+        save_figure('CIPvars',figname,savefig)
     end
-    input([LC ' is displayed. Press Enter key to continue.'])
-    S(k).rhocorr = corrs;
+    if tf_input; input([varDIS{j} ' ' LC ' is displayed. Press Enter key to continue.']); end
+    S(k).([varDIS{j} '_corr']) = corrs;
+end
 end
 
-clear k l t idx aux hdr tnr* LC fltr TTdis TTown TT corrs
+clear j k l t idx aux rows hdr tnr* LC fltr TTdis TTown TT corrs var* plt*
