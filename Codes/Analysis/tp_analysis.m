cf94ce1e18121dceec9709ca63966ab4d5674df6
Pavel Solís <pavel.solis@gmail.com>
2019-05-13 16:32:31 -0400

    Estimates TP for the nominal and synthetic YCs and plots them for comparison

---
+++ Codes/Analysis/tp_analysis.m
@@ -0,0 +1,67 @@
+%% Analysis of Term Premia: EM vs AE, Nominal vs Synthetic
+% This code calls functions to compare the estimated term premia between
+% advanced and emerging countries as well as between nominal and synthetic 
+% yield curves.
+% 
+% m-files called: 
+%
+% Pavel Solís (pavel.solis@gmail.com), May 2019
+%%
+clear
+currentDir = pwd;
+cd '/Users/Pavel/Dropbox/Dissertation/Book-DB-Sync/Ch_Synt-DB/Codes-DB'
+load('struct_data_1_S.mat')
+load('struct_data_2_TT.mat')
+load('struct_data_3_cells.mat')
+cd(currentDir)
+N  = 3;
+dt = 1/12;
+
+S = daily2monthly(dataset_daily,header_daily,S,'LCSYNT');
+[S,corrPCsyn] = tp_estimation(S,N,dt,'LCSYNT');
+
+S = daily2monthly(dataset_daily,header_daily,S,'LC');
+[S,corrPCnom] = tp_estimation(S,N,dt,'LC');
+
+
+% %% Add LCCS to Structure
+% ncntrs   = length(S);
+% tnrs_all = [0; cellfun(@str2num,header_daily(2:end,5))];
+% fltrCIP  = ismember(header_daily(:,2),'CIPDEV');
+% 
+% for k = 1:ncntrs
+%     fltrDEV = ismember(header_daily(:,1),S(k).iso) & fltrCIP;
+%     tnrs    = tnrs_all(fltrYLD);                                        % Tenors available
+%     fltrYLD(1) = true;                                                  % To include dates
+%     data_cipdev = dataset_daily(:,fltrYLD);
+%     data_cipdev    = end_of_month(data_cipdev);                         % Keep end-of-month observations
+%     S(k).(fnames{2}) = [nan tnrs'; data_lc(:,1) data_lc(:,2:end)/100];
+% end
+
+%% Compare TPs of Nominal vs Synthetic
+ncntrs  = length(S);
+fnames  = fieldnames(S);
+tnr     = 10;
+series  = 'yldsP';%'tp';
+rowStrt = 2;
+
+% figure
+% hold on
+for k = 1:ncntrs
+    idxN    = contains(fnames,['nom' series]);   fnameN  = fnames{idxN};
+    idxS    = contains(fnames,['syn' series]);   fnameS  = fnames{idxS};
+    matsN   = S(k).(fnameN)(1,2:end);            matsS   = S(k).(fnameS)(1,2:end);
+    datesN  = S(k).(fnameN)(rowStrt:end,1);      datesS  = S(k).(fnameS)(rowStrt:end,1);
+    seriesN = S(k).(fnameN)(rowStrt:end,2:end);  seriesS = S(k).(fnameS)(rowStrt:end,2:end);
+    
+    figure
+    plot(datesN,seriesN(:,matsN == tnr),datesS,seriesS(:,matsS == tnr))
+%     plot(datesN,seriesN(:,matsN == tnr))
+%     plot(datesS,seriesS(:,matsS == tnr))
+    title([S(k).ccy '  ' num2str(tnr) ' YR']),
+    legend('Nominal','Synthetic')%, ylabel('%')
+    datetick('x','YY:QQ')
+    hline = refline(0,0); hline.Color = 'k';
+    ylim([-inf inf])
+end
+% hold off
