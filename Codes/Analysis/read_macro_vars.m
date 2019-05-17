4ad3552e49e62954404d4cd22b6959c760fe1015
Pavel Solís <pavel.solis@gmail.com>
2019-05-17 14:05:04 -0400

    Updates relative path and file names of xls files, and reads a sheet with GDP surveys for MYR

--- Codes/Analysis/read_macro_vars.m
+++ Codes/Analysis/read_macro_vars.m
@@ -13,6 +13,13 @@ data_macro(:,1) = dates_macro;                  % Use dates in Matlab format
 filename2 = 'Macro_Vars_Tickers.xlsx';
 [~,txt]   = xlsread(filename2,2);
 hdr_macro = txt(:,1:6);
+
+data_myr  = xlsread(filename2,'MYR');           % Read data without headers but with dates
+dates_cty = x2mdate(data_myr(:,1),0);           % Convert dates from Excel to Matlab format
+data_myr(:,1) = dates_cty;
+data_myr  = data_myr(1:end-2,:);                % Interested in first obs; last obs may have NaNs
+data_myr  = data_myr(isnan(data_myr(:,2)),3);   % Extract Bloomberg GDP survey data for missing obs
+
 cd(path)
 
-clear filename* txt path
+clear path txt filename* dates_*
