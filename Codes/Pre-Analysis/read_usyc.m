0adf6a5216342805a66fb76725b0dc145ec91ec5
Pavel Solís <pavel.solis@gmail.com>
2020-04-18 14:07:46 -0400

    Simplification to define categorical variables: tenor in last column initially and relocated after categorical conversion of the other columns

--- Codes/Pre-Analysis/read_usyc.m
+++ Codes/Pre-Analysis/read_usyc.m
@@ -30,10 +30,9 @@ TTusyc = synchronize(TTbill,TTgsw);                                           % 
 
 % Header
 H_usyc  = construct_hdr('USD','HC',TTusyc.Properties.VariableNames',...       % variable names as tickers
-    strcat('USD ZERO-COUPON YIELD',{' '},tnrs,' YR'),num2cell(matall),' ','GSW'); % HC - hard currency
+    strcat('USD ZERO-COUPON YIELD',{' '},tnrs,' YR'),' ','GSW',num2cell(matall)); % HC - hard currency
 % H_usyc(1:3,end) = {'CRSP'};                                                 % if Tbill data not from GSW
-THusyc = cell2table(H_usyc);
-THusyc = movevars(THusyc,5,'After',7);                                        % tenor remains as double
+THusyc = cell2table(H_usyc);                                                  % tenor (as double) is last
 THusyc = [varfun(@categorical,THusyc,'inputvariables',THusyc.Properties.VariableNames(1:6)) THusyc(:,7)];
 THusyc = movevars(THusyc,7,'After',4);                                        % relocate tenor
 THusyc.Properties.VariableNames = {'Currency','Type','Ticker','Name','Tenor','FloatingLeg','Source'};
@@ -41,5 +40,5 @@ THusyc.Properties.VariableNames = {'Currency','Type','Ticker','Name','Tenor','Fl
 % In case the NSS parameters want to be included in the dataset
 % TTusyc = synchronize(TTparm,TTusyc);
 % H_prms  = construct_hdr('USD','PARAMETER',TTparm.Properties.VariableNames',...
-%     'USD N-S-S YIELD CURVE',NaN,' ','GSW');
+%     'USD N-S-S YIELD CURVE',' ','GSW',NaN);
 % THusyc = cell2table([H_prms; H_usyc]);
