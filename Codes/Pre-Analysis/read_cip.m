4d59e2201c78c2553bbb47f97c8417b986a74d5e
Pavel Solís <pavel.solis@gmail.com>
2020-04-20 10:37:41 -0400

    Script converted into a function

--- Codes/Pre-Analysis/read_cip.m
+++ Codes/Pre-Analysis/read_cip.m
@@ -1,25 +1,20 @@
-%% Read CIP
-% This code reads data as a Matlab table and timetable from 'original_CIP_Data.xlsx'
-% of Du, Im & Schreger (2018).
-%
-% Pavel Solís (pavel.solis@gmail.com), March 2019
+function [TTcip,currEM,currAE] = read_cip()
+% READ_CIP Read CIP data from Du, Im & Schreger (2018)
+%   TTcip: stores historical data
+%   currEM: contains currencies of emerging market in ascending order
+%   currAE: contains currencies of advanced countries in ascending order
+
+% Pavel Solís (pavel.solis@gmail.com), April 2020
 %%
-path = pwd;
-cd(fullfile(path,'..','..','Data','Raw'))           % Use platform-specific file separators
-filenameCIP = 'original_CIP_Data.xlsx';
-T_cip = readtable(filenameCIP,'Sheet',1,'ReadVariableNames',true,'DatetimeType','exceldatenum');
-cd(path)
+pathc  = pwd;
+pathd  = fullfile(pathc,'..','..','Data','Raw');                  % platform-specific file separators
+cd(pathd)
+namefl = 'CIP_Data.xlsx';
+opts   = detectImportOptions(namefl,'Sheet',1);
+opts   = setvartype(opts,opts.VariableNames([1:2 4]),'categorical');
+TTcip  = readtimetable(namefl,opts);
+cd(pathc)
 
-% Convert variables as cell arrays to categorical variables
-T_cip.group    = categorical(T_cip.group);
-T_cip.currency = categorical(T_cip.currency);
-T_cip.tenor    = categorical(T_cip.tenor);
-
-% Relocate dates and change to Matlab format
-T_cip      = movevars(T_cip,'date','Before',1);     % Relocate dates in first column
-T_cip.date = x2mdate(T_cip.date,0,'datetime');      % Convert dates from Excel to Matlab format
-
-% Timetable
-TT_cip     = table2timetable(T_cip);                % Convert table to a timetable
-
-clear path filenameCIP
+[~,grp,currencies] = findgroups(TTcip.group,TTcip.currency);
+currEM = cellstr(currencies(grp == 'eme'));
+currAE = cellstr(currencies(grp == 'g10'));
