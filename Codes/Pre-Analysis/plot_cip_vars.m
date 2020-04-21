6d70ac919aef5ce47060ad5fa1c3aeac76d9c77a
Pavel Solís <pavel.solis@gmail.com>
2020-04-21 09:41:55 -0400

    Updated: figures saved as eps only in Spreads folder, 1- 5- 10Y plots, correlation matrix for group, spread, year

--- Codes/Pre-Analysis/plot_cip_vars.m
+++ Codes/Pre-Analysis/plot_cip_vars.m
@@ -1,87 +1,98 @@
-%% Plot CIP Variables
-% This code plots the forward premium, local and foreign interest rate spreads
-% and deviations from covered interest rate parity for different countries.
+%% Plot Spreads
+% This code plots local and foreign currency interest rate spreads, the forward
+% premium and deviations from covered interest rate parity for countries in sample.
 % Assumes that header_daily and dataset_daily are in the workspace.
-% m-files called: save_figure.m
-%
-% Pavel Solís (pavel.solis@gmail.com), April 2019
+
+% m-files called: save_figure
+% Pavel Solís (pavel.solis@gmail.com), April 2020
 %%
-if ~exist('T_cip','var')                                % Run code if T_cip is not in the workspace
-    run read_cip.m
+if ~exist('currEM','var')                                % if currEM is not in the workspace
+    [~,currEM,currAE] = read_cip();
 end
 
-[LCs,currEM,currAE] = read_currencies(T_cip);
-dates   = dataset_daily(:,1);
-figdir = 'CIPvars'; figsave = false;
+LCs    = [currEM;currAE];
+dates  = dataset_daily(:,1);
+figdir = 'Spreads'; figsave = true;
 
 %% Spreads per maturity for each country
 types  = {'RHO','CIPDEV','LCSPRD','FCSPRD'};
-year   = '5';
-fltrTP = ismember(header_daily(:,2),types);             % To report the specific types
-fltrYR = ismember(header_daily(:,5),year);              % To report a specific year
-for k = 1:numel(LCs)
-    fltrLC  = ismember(header_daily(:,1),LCs{k});
-    fltr    = fltrLC & fltrTP & fltrYR;                 % Criteria to meet
-    sprds   = dataset_daily(:,fltr);
-    sprdsMA = movmean(sprds,10);                        % To report results as in Du & Schreger (2016)
-    labels  = header_daily(fltr,2);                     % Types available for the currency
-    figure
-    plot(dates,sprdsMA)
-    title(['Spreads: ' LCs{k} ' ' year 'Y'])
-    ylabel('%')
-    legend(labels)
-    datetick('x','yy','keeplimits')                     % Annual ticks
-    figname = ['sprds_' LCs{k} '_' year 'y'];
-    save_figure(figdir,figname,figsave)
+years  = {'1','5','10'};
+fltrTP = ismember(header_daily(:,2),types);             % report specific types
+for k0 = 1:numel(years)
+    fltrYR = ismember(header_daily(:,5),years{k0});    	% report a specific year
+    for k1 = 1:numel(LCs)
+        fltrLC  = ismember(header_daily(:,1),LCs{k1});
+        fltr    = fltrLC & fltrTP & fltrYR;            	% criteria to meet
+        sprds   = dataset_daily(:,fltr);
+        sprdsMA = movmean(sprds,10);                   	% to report results as in Du & Schreger (2016)
+        labels  = header_daily(fltr,2);                	% types available for the currency
+        figure
+        plot(dates,sprdsMA);
+        title(['Spreads: ' LCs{k1} ' ' years{k0} 'Y'])
+        ylabel('%')
+        legend(labels,'AutoUpdate','off')
+        datetick('x','yy','keeplimits')              	% annual ticks
+        yline(0);                                       % horizontal line at the origin
+        figname = ['sprds_' years{k0} 'y_' LCs{k1}];
+        save_figure(figdir,figname,'eps',figsave)
+    end
 end
 
 %% Spreads per country across maturities
 keydates = datenum(['30-Sep-2008';'31-May-2013';'30-Nov-2016']);
-fltrYR   = ismember(header_daily(:,5),{'1','5','10'});  % To report the specific years
-for j = 1:numel(types)
-    fltrTP = ismember(header_daily(:,2),types{j});      % To report a specific type
-    for k = 1:numel(LCs)
-        fltrLC = ismember(header_daily(:,1),LCs{k});
-        fltr   = fltrLC & fltrTP & fltrYR;              % Criteria to meet
-        if sum(fltr) > 0                                % Some countries don't have the specified type
+fltrYR   = ismember(header_daily(:,5),years);           % report specific years
+for j0 = 1:numel(LCs)
+    fltrLC = ismember(header_daily(:,1),LCs{j0});
+    for j1 = 1:numel(types)
+        fltrTP = ismember(header_daily(:,2),types{j1}); % report a specific type
+        fltr   = fltrLC & fltrTP & fltrYR;              % criteria to meet
+        if sum(fltr) > 0                                % some countries don't have the specified type
             sprds   = dataset_daily(:,fltr);
             sprdsMA = movmean(sprds,10);
-            labels  = strcat(header_daily(fltr,5),'Y'); % Tenors of the spreads
+            labels  = strcat(header_daily(fltr,5),'Y'); % tenors of the spreads
             figure
-            p1 = plot(dates,sprdsMA);
-            line(repmat(keydates,1,2),get(gca,'YLim'),'Color',[0 0 0]+0.65,'LineStyle','-.');
-            legend(p1,labels,'Location','best','Orientation','horizontal')
-            title([types{j} ' ' LCs{k}])
+            plot(dates,sprdsMA);
+            legend(labels,'Location','best','Orientation','horizontal','AutoUpdate','off')
+            title([LCs{j0} ' ' types{j1}])
             ylabel('%')
             datetick('x','yy','keeplimits')
-            figname = ['TS_' types{j} '_' LCs{k}];      % Term structure per type
-            save_figure(figdir,figname,figsave)
+            line(repmat(keydates,1,2),get(gca,'YLim'),'Color',[0 0 0]+0.65,'LineStyle','-.');
+            yline(0);                                   % horizontal line at the origin
+            figname = ['ts_' LCs{j0} '_' types{j1}];    % term structure per type
+            save_figure(figdir,figname,'eps',figsave)
         end
     end
 end
 
 %% Spreads per maturity across countries
-% To see whether CIP deviations are correlated across countries in the sample
+% To see whether spreads are correlated across countries in the sample
 
-type     = 'CIPDEV';
-group    = {currEM,currAE};
-corrmtrx = cell(length(group),2);
-fltrTY   = ismember(header_daily(:,2),type) & ismember(header_daily(:,5),year);
-for k = 1:length(group)
-    fltrGP  = ismember(header_daily(:,1),group{k});
-    fltr    = fltrGP & fltrTY;
-    sprds   = dataset_daily(:,fltr);
-    sprdsMA = movmean(sprds,10);
-    labels  = header_daily(fltr,1);                     % Countries
-    figure
-    plot(dates,sprdsMA)
-    title([type ' ' year 'Y'])
-    ylabel('%')
-    legend(labels,'Location','best','Orientation','horizontal','NumColumns',5)
-    datetick('x','yy','keeplimits')
-    figname = ['G' k '_' type '_' year 'y'];
-    save_figure(figdir,figname,figsave)
-    [corrmtrx{k,1},corrmtrx{k,2}] = corrcoef(sprds,'Rows','complete');  % Omits rows with NaN values
+types = {'RHO','CIPDEV'};
+group = {currEM,currAE};
+n0    = numel(types);   n1 = length(group);  n2 = numel(years);
+corrmtrx = cell(n2,n0*n1,2);
+for l0 = 1:n0
+    for l1 = 1:n1
+        fltrGP  = ismember(header_daily(:,1),group{l1});
+        for l2 = 1:n2
+            fltrTY  = ismember(header_daily(:,2),types{l0}) & ismember(header_daily(:,5),years{l2});
+            fltr    = fltrGP & fltrTY;
+            sprds   = dataset_daily(:,fltr);
+            sprdsMA = movmean(sprds,10);
+            labels  = header_daily(fltr,1);            	% countries
+            figure
+            plot(dates,sprdsMA)
+            title(['G' num2str(l1) ': ' types{l0} ' ' years{l2} 'Y'])
+            ylabel('%')
+            legend(labels,'Location','best','Orientation','horizontal','NumColumns',5,'AutoUpdate','off')
+            datetick('x','yy','keeplimits')
+            yline(0);                                 	% horizontal line at the origin
+            figname = ['g' num2str(l1) '_' types{l0} '_' years{l2} 'y'];
+            save_figure(figdir,figname,'eps',figsave)
+            [corrmtrx{l2,n0*(l0-1)+l1,1},corrmtrx{l2,n0*(l0-1)+l1,2}] = corrcoef(sprds,'Rows','complete');
+            % rows (1Y,5Y,10Y), cols (RHO (G1,G2),CIP (G1,G2)), 3D (correlations omit NaN rows, p-values)
+        end
+    end
 end
 
-clear j k p1 LCs curr* type* year* fltr* sprds* fig* dates keydates labels
+clear LCs curr* type* year* fltr* sprds* fig* dates keydates labels group j* k* l* n*
