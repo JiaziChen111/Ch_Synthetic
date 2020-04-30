0e788457cc04004444eb3e4f25ab39c9cd55f104
Pavel Solís <pavel.solis@gmail.com>
2020-04-30 16:29:04 -0400

    Output argument added to report fit. Use 1 and end instead of find for initial values of b0 and b1

--- Codes/Pre-Analysis/zc_yields.m
+++ Codes/Pre-Analysis/zc_yields.m
@@ -1,4 +1,4 @@
-function [data_zc,hdr_zc] = zc_yields(dataset,header,curncs,tfplot)
+function [data_zc,hdr_zc,fitrprt] = zc_yields(dataset,header,curncs,tfplot)
 % ZC_YIELDS Zero-coupon continuosly compounded local currency (LC) yield curves
 % BFV and IYC LC curves report coupon-equivalent (CE) par and zero-coupon yields,
 % they are converted to zero-coupon continuosly compounded (CC) yields which
@@ -6,6 +6,7 @@ function [data_zc,hdr_zc] = zc_yields(dataset,header,curncs,tfplot)
 % IYC curves used for BRL and ILS, BFV curves used for all other countries
 %   data_zc: stores historical data
 %   hdr_zc: stores headers (note: no title row, i.e. ready to be appended)
+%   fitrprt: reports fit (country, average RMSE in %, minutes taken to fit)
 
 % m-files called: fltr4tickers, construct_hdr
 % Pavel Solís (pavel.solis@gmail.com), April 2020
@@ -16,8 +17,11 @@ data_zc = dataset(:,1);
 settle  = dataset(:,1);
 tmax    = 10;
 frqCE   = 2;                                                    % CE compounding frequency: 2 - semiannual
+tnrsout = [0.25 0.5 1:10]';
+tnrshdr = cellfun(@num2str,num2cell(tnrsout),'UniformOutput',false);
+fitrprt = cell(numel(curncs),3);
 for k0  = 1:numel(curncs)
-tic
+    tic
     LC  = curncs{k0};	tfbfv = true;
     [fltr,tnrscll] = fltr4tickers(LC,'LC','',header);
     
@@ -37,21 +41,22 @@ tic
     tnrsnum(~ttrue) = [];   tnrscll(~ttrue) = [];   ftrue(~ttrue) = [];
     fltr(:) = false;        fltr(ftrue)     = true;
     if (strcmp(LC,'HUF') && strcmp(tnrscll{end},'20'))          % in case tmax >= 20
-        fltr(find(fltr,1,'last')) = false; tnrscll(end) = [];
+        fltr(find(fltr,1,'last')) = false; %tnrscll(end) = [];
     end
     
     % Extract information and preallocate variables
     yldsCE = dataset(:,fltr)/100;                               % in decimals
     [ndates,ntnrs] = size(yldsCE);
-    yldszc = nan(ndates,ntnrs);    ydates  = nan(ndates,ntnrs);    rmse = nan(ndates,1);   params = [];
+    ydates = nan(ndates,ntnrs);    rmse = nan(ndates,1);   params = [];
+    yldszc = nan(ndates,length(tnrsout));
     
     % Fit NS/S models daily
     for k1 = 1:ndates
         fltry = ~isnan(yldsCE(k1,:));                           % tenors with observations
         if sum(fltry) > 0                                       % at least one observation
             % Tenors and maturities (based on settlement day)
-            tnrs = tnrsnum(fltry);                              % define tenors
-            ydates(k1,fltry) = datemnth(settle(k1),12*tnrs);    % define maturity dates
+            tnrsin = tnrsnum(fltry);                          	% define tenors
+            ydates(k1,fltry) = datemnth(settle(k1),12*tnrsin); 	% define maturity dates
             
             % Yields treatment depending on whether BFV or IYC curve  (column vectors)
             if tfbfv == true                                  	% BFV par yields CE to zc yields CC
@@ -72,18 +77,18 @@ tic
             end
             
             % Initial values from the data
-            fmax  = find(fltry,1,'last' );  fmin  = find(fltry,1,'first');
-            beta0 = yzc2fit(fmax);          beta1 = yzc2fit(fmin) - beta0;  beta2 = -beta1;
-            beta3 = beta2;                  tau1  = 1;                      tau2  = tau1;
+            beta0 = yzc2fit(end);       beta1 = yzc2fit(1) - beta0;     beta2 = -beta1;
+            beta3 = beta2;             	tau1  = 1;                      tau2  = tau1;
+            paramsdata = [beta0 beta1 beta2 beta3 tau1 tau2];
             
             % Fit NS/S models
-            [yzcfitted,params,error] = fit_NS_S(yzc2fit,tnrs,params,[beta0 beta1 beta2 beta3 tau1 tau2]);
-            yldszc(k1,fltry) = yzcfitted*100;                   % in percentages
-            rmse(k1) = error*100;
+            [yzcfitted,params,error] = fit_NS_S(yzc2fit,tnrsin,tnrsout,params,paramsdata);
+            yldszc(k1,:) = yzcfitted*100;                       % in percentages
+            rmse(k1)     = error*100;
             
             % Plot and compare
             if tfplot
-            plot(tnrs,yzc2fit*100,'b',tnrs,yldszc(k1,fltry),'r',tnrs,yldsCE(k1,fltry)*100,'mo')
+            plot(tnrsin,yzc2fit*100,'b',tnrsout,yldszc(k1,:),'r',tnrsin,yldsCE(k1,fltry)*100,'mo')
             title([LC '  ' datestr(settle(k1))])
             H(k1) = getframe(gcf);                              % imshow(H(2).cdata) for a frame
             end
@@ -91,23 +96,23 @@ tic
     end
     
     % Save and append data
-    % ['RMSE for ' LC ': ' num2str(mean(rmse,'omitnan'))]         % report fit
-    name_ZC = strcat(LC,' NOMINAL LC YIELD CURVE',{' '},tnrscll,' YR');
-    hdr_ZC  = construct_hdr(LC,'LCNOM','N/A',name_ZC,tnrscll,' ',' ');
+    secs2fit = toc;
+    fitrprt{k0,1} = LC; fitrprt{k0,2} = mean(rmse,'omitnan'); fitrprt{k0,3} = secs2fit/60;
+    name_ZC = strcat(LC,' NOMINAL LC YIELD CURVE',{' '},tnrshdr,' YR');
+    hdr_ZC  = construct_hdr(LC,'LCNOM','N/A',name_ZC,tnrshdr,' ',' ');
     hdr_zc  = [hdr_zc; hdr_ZC];
     data_zc = [data_zc, yldszc];
-toc
 end
 end
 
-function [yldszc,params1model,rmse] = fit_NS_S(yzc2fit,tnrs,params0model,params1data)
+function [yldszc,params1model,rmse] = fit_NS_S(yzc2fit,tnrsin,tnrsout,params0model,params1data)
 % FIT_NS_S Return zero-coupon yields yldszc after fitting NS (if max(tnrs) <= 10Y)
 % or NSS (if max(tnrs) > 10Y) model to yzc2fit taking params1data (of length 6) and,
 % if available, params0model (of length 4 or 6) as initial values
 
 options = optimoptions('lsqcurvefit','Display','off'); lb = []; ub = [];
 if size(yzc2fit,1) ~= 1; yzc2fit = yzc2fit'; end            % ensure yzc2fit is a row vector
-if isempty(params0model) || (length(params0model) == 4 && max(tnrs) > 10)
+if isempty(params0model) || (length(params0model) == 4 && max(tnrsin) > 10)
     init_vals = [];                                         % first iteration or change from NS to NSS
 else                                                        % previous iteration was either NS or NSS
     init_vals = params0model;                               % use parameters from previous iteration
@@ -115,12 +120,12 @@ end                                                         % note: size(init_va
 vrmse = nan(size(init_vals,1)+1,1);                         % size(rmse,1) = number of initial values
 
 % Fit NS or NSS model
-if max(tnrs) <= 10                                          % fit NS model
+if max(tnrsin) <= 10                                       	% fit NS model
     init_vals = [init_vals; params1data(1) params1data(2) params1data(3) params1data(5)];
     vparams   = nan(size(init_vals));                       % size(init_vals,2) = 4
     for j0 = 1:size(init_vals,1)                            % at least one, at most two times
         try                                                 % exagerate rmse if init_vals yield error
-            [prms,~,res]  = lsqcurvefit(@y_NS,init_vals(j0,:),tnrs,yzc2fit,lb,ub,options);
+            [prms,~,res]  = lsqcurvefit(@y_NS,init_vals(j0,:),tnrsin,yzc2fit,lb,ub,options);
             vparams(j0,:) = prms;
             vrmse(j0)     = sqrt(mean(res.^2));
         catch
@@ -130,13 +135,13 @@ if max(tnrs) <= 10                                          % fit NS model
     end
     [rmse,idx]   = min(vrmse);                              % identify best fit
     params1model = vparams(idx,:);                          % choose best fit
-    yldszc       = y_NS(params1model,tnrs);                 % extract yields
+    yldszc       = y_NS(params1model,tnrsout);             	% extract yields
 else                                                        % fit NSS model
     init_vals = [init_vals; params1data];                   % size(init_vals,2) = 6
     vparams   = nan(size(init_vals));
     for j0 = 1:size(init_vals,1)
         try
-            [prms,~,res]  = lsqcurvefit(@y_NSS,init_vals(j0,:),tnrs,yzc2fit,lb,ub,options);
+            [prms,~,res]  = lsqcurvefit(@y_NSS,init_vals(j0,:),tnrsin,yzc2fit,lb,ub,options);
             vparams(j0,:) = prms;
             vrmse(j0)     = sqrt(mean(res.^2));
         catch
@@ -146,6 +151,6 @@ else                                                        % fit NSS model
     end
     [rmse,idx]   = min(vrmse);
     params1model = vparams(idx,:);
-    yldszc       = y_NSS(params1model,tnrs);
+    yldszc       = y_NSS(params1model,tnrsout);
 end
 end
