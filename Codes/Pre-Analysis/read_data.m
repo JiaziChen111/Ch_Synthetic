89b644fe66731f72b3116b9d4e9d5c12754d4830
Pavel Solís <pavel.solis@gmail.com>
2020-05-03 18:02:31 -0400

    Leave in dataset only types of variables generated (e.g. LCNOM, LCSYNT), update variable names

--- Codes/Pre-Analysis/read_data.m
+++ Codes/Pre-Analysis/read_data.m
@@ -4,10 +4,11 @@
 % and deviations from covered interest rate parity.
 
 % m-files called: read_platforms, read_usyc, fwd_prm, spreads, read_cip,
-% plot_spreads, append_dataset, iso2names
+% plot_spreads, compare_cip, append_dataset, iso2names
 % Pavel Solís (pavel.solis@gmail.com), April 2020
 %% Data on yield curves and swap curves
-clear; clc; close all;
+clear; clc; close all; tStart = tic;
+warning('OFF','MATLAB:table:ModifiedAndSavedVarnames')                          % suppress table warnings
 [TTpltf,THpltf] = read_platforms();
 [TTusyc,THusyc] = read_usyc();
 TTdy = synchronize(TTpltf,TTusyc,'commonrange');                                % union over intersection
@@ -19,28 +20,36 @@ header_daily(2:end,5) = cellfun(@num2str,header_daily(2:end,5),'UniformOutput',f
 dataset_daily = timetable2table(TTdy);                                                  % data to table
 dataset_daily = table2cell([num2cell(datenum(TTdy.Date)), dataset_daily(:,2:end)]);     % date to datenum
 dataset_daily = cell2mat(dataset_daily);
+curncs = cellstr(unique(THdy.Currency(ismember(THdy.Type,'SPT')),'stable'));
+clear T*
 
 %% Data on forward premiums 
-curncs = cellstr(unique(THdy.Currency(ismember(THdy.Type,'SPT')),'stable'));
-[data_fp,hdr_fp,tnrsLCfp]    = fwd_prm(dataset_daily,header_daily,curncs);
+[data_fp,hdr_fp,tnrs_fp]     = fwd_prm(dataset_daily,header_daily,curncs);
 [dataset_daily,header_daily] = append_dataset(dataset_daily,data_fp,header_daily,hdr_fp);
 
-%% Zero-coupon yield curves
-[data_zc,hdr_zc,fitreport]   = zc_yields(dataset_daily,header_daily,curncs,false);
+%% Data on nominal yield curves
+[data_zc,hdr_zc,fit_zc]      = zc_yields(dataset_daily,header_daily,curncs,false);
 [dataset_daily,header_daily] = append_dataset(dataset_daily,data_zc,header_daily,hdr_zc);
 
 %% Data on spreads (synthetic yield curves, interest rate differentials, CIP deviations)
-[data_sprd,hdr_sprd,tnrsspd] = spreads(dataset_daily,header_daily);
-[dataset_daily,header_daily] = append_dataset(dataset_daily,data_sprd,header_daily,hdr_sprd);
+[data_sprd,hdr_sprd,tnrs_spd] = spreads(dataset_daily,header_daily);
+[dataset_daily,header_daily]  = append_dataset(dataset_daily,data_sprd,header_daily,hdr_sprd);
 
+%% Clean dataset
+types = {'Type','RHO','LCNOM','LCSYNT','LCSPRD','CIPDEV','FCSPRD'};
+fltr = ~ismember(header_daily(:,2),types);
+dataset_daily(:,fltr) = [];
+header_daily(fltr,:)  = [];
+
+%% Assess series
 [TTcip,currEM,currAE] = read_cip();
-% corrsprd = plot_spreads(dataset_daily,header_daily,currEM,currAE);
-% Scorr    = compare_cip(dataset_daily,header_daily,curncs,TTcip);
-
-%%
+figstop  = false;	figsave = false;
+corrsprd = plot_spreads(dataset_daily,header_daily,currEM,currAE,figstop,figsave);
+corrDIS  = compare_cip(dataset_daily,header_daily,curncs,TTcip,figstop,figsave);
 S = cell2struct(iso2names(curncs)',{'cty','ccy','iso','imf'});
-clear data_* hdr_* TH* TTpltf THusyc TTdy
+clear data_* hdr_* fig* fltr types
+toc(tStart)
 
 %% Save variables in mat files (in Dropbox, not in Git directory)
-% save struct_data_1_S.mat S Scorr curncs currAE currEM Tcip corrsprd tnrs*
-% save struct_data_3_cells.mat dataset_daily header_daily
+% save struct_data_1_S.mat S fit_zc corr* cur* tnrs*
+% save struct_data_2_cells.mat dataset_daily header_daily
