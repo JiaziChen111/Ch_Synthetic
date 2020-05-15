c6cd89f341f5102025fa56462f4178e5a91d9b43
Pavel Solís <pavel.solis@gmail.com>
2020-05-15 18:51:59 -0400

    Output is monthly macro data (reads macro vars and calls read_cbpol, synchronize the two datasets)

--- Codes/Analysis/read_macrovars.m
+++ Codes/Analysis/read_macrovars.m
@@ -1,5 +1,5 @@
-function [data_macro,hdr_macro] = read_macrovars()
-% READ_MACROVARS Read macroeconomic and financial data retrieved from Bloomberg
+function [data_macro,hdr_macro] = read_macrovars(S)
+% READ_MACROVARS Read macroeconomic, financial and policy rate data
 %   data_macro: stores historical data
 %   hdr_macro: stores headers
 
@@ -9,17 +9,33 @@ pathc  = pwd;
 pathd  = fullfile(pathc,'..','..','Data','Raw');                            % platform-specific file separators
 namefl = {'Macro_Vars_Tickers.xlsx','Macro_Vars_Data.xlsx'};
 
+% Read macro data from Bloomberg
 cd(pathd)
-hdr_macro  = readcell(namefl{1},'Sheet','Tickers');                         % read headers
-hdr_macro  = hdr_macro(:,1:6);                                              % remove extra columns
-data_macro = readmatrix(namefl{2},'Sheet','All');                           % read macro variables
-data_macro(:,1) = x2mdate(data_macro(:,1),0);                               % dates from Excel to Matlab format
+hdr_mcr  = readcell(namefl{1},'Sheet','Tickers');                           % read headers
+hdr_mcr  = hdr_mcr(:,1:6);                                                  % remove extra columns
+data_mcr = readmatrix(namefl{2},'Sheet','All');                             % read macro variables
+datesmcr = x2mdate(data_mcr(:,1),0);                                        % dates from Excel to Matlab format
+data_mcr(:,1) = datesmcr;
+datesmonth = unique(lbusdate(year(datesmcr),month(datesmcr)));              % last U.S. business day per month
+data_mcr = data_mcr(ismember(datesmcr,datesmonth),:);                       % extract monthly dataset
 
 % MYR GDP case (correlation between survey and actual series of 0.75)
 data_myr = readmatrix(namefl{2},'Sheet','MYR');                          	% read GDP and Bloomberg survey data
 data_myr(isnan(data_myr(:,2)),2) = data_myr(isnan(data_myr(:,2)),3);        % use survey data for missing obs
-fltrMYR  = contains(hdr_macro(:,3),'MAGDHIY');                            	% identify quarterly GDP for MYR
+fltrMYR  = contains(hdr_mcr(:,3),'MAGDHIY');                            	% identify quarterly GDP for MYR
 datesmyr = x2mdate(data_myr(:,1),0);                                        % dates from Excel to Matlab format
 datesmyr = unique(lbusdate(year(datesmyr),month(datesmyr)));                % use last business day of quarter
-data_macro(ismember(data_macro(:,1),datesmyr),fltrMYR) = data_myr(:,2);     % use constructed quarterly GDP data
+data_mcr(ismember(data_mcr(:,1),datesmyr),fltrMYR) = data_myr(:,2);         % use constructed quarterly GDP data
 cd(pathc)
+
+% Read BIS policy rates
+[data_cbpol,hdr_cbpol] = read_cbpol(S);
+
+% Merge datasets
+TT1 = array2timetable(data_mcr(:,2:end),'RowTimes',datetime(data_mcr(:,1),'ConvertFrom','datenum'));
+TT2 = array2timetable(data_cbpol(:,2:end),'RowTimes',datetime(data_cbpol(:,1),'ConvertFrom','datenum'));
+TT  = synchronize(TT1,TT2,'intersection');
+data_macro = timetable2table(TT);                                         	% data to table
+data_macro = table2cell([num2cell(datenum(data_macro{:,1})) data_macro(:,2:end)]);	% date to datenum
+data_macro = cell2mat(data_macro);
+hdr_macro  = [hdr_mcr; hdr_cbpol];
