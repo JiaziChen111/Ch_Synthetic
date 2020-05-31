89344a9ea7cb2071608d74a4d189a1d1ea5da8c3
Pavel Solís <pavel.solis@gmail.com>
2020-05-31 18:43:58 -0400

    New inputs (maturities and dt) needed by atsm_params, update order of inputs and outputs in Kfs and atsm_params, respectively

--- Codes/Analysis/llkfn.m
+++ Codes/Analysis/llkfn.m
@@ -1,9 +1,9 @@
-function llk = llkfn(parest,y,x00,P00)
+function llk = llkfn(parest,y,x00,P00,maturities,dt)
 % LLKFN Return the negative log-likelihood computed after the Kalman filter
 
 % m-files called: atsm_params, Kfs
 % Pavel Solís (pavel.solis@gmail.com), May 2020
 %%
-[Phi,A,Q,R,mu_x,mu_y] = atsm_params(parest);            % get model parameters
-llk = Kfs(y,Phi,A,Q,R,x00,P00,mu_x,mu_y);               % calculate the log-likelihood
-llk = -llk;                                             % return minus log-likelihood
+[mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,maturities,dt);	% get model parameters
+llk = Kfs(y,mu_x,mu_y,Phi,A,Q,R,x00,P00);                   % calculate the log-likelihood
+llk = -llk;                                                 % return minus log-likelihood
