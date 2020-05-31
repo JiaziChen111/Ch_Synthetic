80e978af7cbe72808b598e086a3a40eb922ff2eb
Pavel Solís <pavel.solis@gmail.com>
2020-05-31 18:47:01 -0400

    Calls parest2vars.m and loadings4ylds.m to prepare the coefficients for the ATSM, order of output in line w/ Kfs

--- Codes/Analysis/atsm_params.m
+++ Codes/Analysis/atsm_params.m
@@ -1,20 +1,38 @@
-function [Phi,A,Q,R,mu_x,mu_y] = atsm_params(parest)
+function [mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,maturities,dt)
 % ATSM_PARAMS Define parameters for affine term structure model
+% Parameters vectorized in parest: PhiP;Sgm;lmbd1;lmbd0;mu_xP;rho1;rho0;sgmY;sgmS
 
+% m-files called: parest2vars, loadings4ylds
 % Pavel Solís (pavel.solis@gmail.com), May 2020
 %%
-% Define parameters of the model not being estimated
-mu_y = zeros(1,1);
-mu_x = zeros(4,1);
-A    = [1,1,0,0];
-Q    = zeros(4); 
-Phi  = zeros(4);
-Phi(2,:) = [0,-1,-1,-1];
-Phi(3,:) = [0,1,0,0];
-Phi(4,:) = [0,0,1,0];
+% Identify maturities of yields and surveys
+matsY = maturities(1:8);                                        % maturities of yields
+matsS = maturities(9:end);                                      % maturities of surveys
+matsF = matsS + 3;                                              % mats2 + tenor (in months) of rate in surveys
+q1    = length(matsY);                                          % q = q1 + q2
+q2    = length(matsS);
 
-% Define parameters of the model to be estimated
-Phi(1,1) = parest(1);
-Q(1,1)   = parest(2);
-Q(2,2)   = parest(3);
-R        = parest(4);
+[PhiP,Sgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest);
+
+% Loadings for yields
+mu_xQ = mu_xP - Sgm*lmbd0;
+PhiQ  = PhiP  - Sgm*lmbd1;
+[AnQ,BnQ] = loadings4ylds(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);   % AnQ: 1*q1, BnQ: p*q1
+
+% Loadings for forward rates (to match survey forecasts)
+[AnE,BnE] = loadings4ylds(matsS,mu_xP,PhiP,zeros(size(Sgm)),rho0,rho1,dt);
+[AmE,BmE] = loadings4ylds(matsF,mu_xP,PhiP,zeros(size(Sgm)),rho0,rho1,dt);
+AnmE = (matsF.*AmE - matsS.*AnE)./(matsF - matsS);
+BnmE = (matsF.*BmE - matsS.*BnE)./(matsF - matsS);
+
+% Loadings for yields and forward rates
+AnQE = [AnQ,AnmE];                                              % 1*q = [1*q1 1*q2]
+BnQE = [BnQ,BnmE];                                              % p*q = [p*q1 p*q2]
+
+% Parameters in state space form
+mu_x = mu_xP;                                                   % p*1
+mu_y = AnQE';                                                   % q*1
+Phi  = PhiP;                                                    % p*p
+A    = BnQE';                                                   % q*p
+Q    = Sgm;                                                     % p*p
+R    = diag([repmat(sgmY,q1,1); repmat(sgmS,q2,1)]);            % q*q
