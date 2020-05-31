53b96b3cfdb88e106a11ca7c58caa1da63fd7546
Pavel Solís <pavel.solis@gmail.com>
2020-05-31 18:50:27 -0400

    Creates dataset of US YC w/ SPF T-bill surveys 1972-2010. Estimate ATSM w/ JSZ (yields only) and w/ Kfs (yields+surveys). Compute TP from both

---
+++ Codes/Analysis/usyc_analysis.m
@@ -0,0 +1,141 @@
+function usyc_analysis()
+% USYC_ANALYSIS Reproduce analysis of Guimaraes (2014)
+
+% m-files called: y_NSS, Kfs
+% Pavel Solís (pavel.solis@gmail.com), May 2020
+% 
+%% Extract data
+pathc    = pwd;
+pathd{1} = fullfile(pathc,'..','..','Data','Raw');                         % platform-specific file separators
+pathd{2} = fullfile(pathc,'..','..','Data','Aux');
+namefl   = {'US_Yield_Curve_Data.xlsx','Mean_TBILL_1Q-4Q-10Y.xlsx'};
+
+for k = 1:2
+    cd(pathd{k})
+    opts = detectImportOptions(namefl{k});
+    opts = setvartype(opts,opts.VariableNames(1),'datetime');
+    opts = setvartype(opts,opts.VariableNames(2:end),'double');
+    opts.VariableNames{1} = 'Date';
+    if k == 1
+        ttaux = readtimetable(namefl{k},opts);
+    else
+        ttsvy = readtimetable(namefl{k},opts);
+    end
+    cd(pathc)
+end
+
+% Yields
+date1   = datetime(datenum('1-Jan-1972'),'ConvertFrom','datenum');
+dateco  = datetime(datenum('1-Jan-1983'),'ConvertFrom','datenum');
+date2   = datetime(datenum('1-Dec-2010'),'ConvertFrom','datenum');
+ttaux   = ttaux(ttaux.Date >= date1,:);                                         % start of sample
+TTgsw   = removevars(ttaux,~contains(ttaux.Properties.VariableNames,'SVENY'));	% keep zero-coupon yields
+TTgsw   = TTgsw(:,[1:5 7 10]);                                                  % keep tenors 1Y:5Y, 7Y, 10Y
+TTparm  = removevars(ttaux,~contains(ttaux.Properties.VariableNames,{'BETA','TAU'})); % keep NSS parameters
+TTbill1 = array2timetable(y_NSS(TTparm{:,:},0.25),'RowTimes',TTparm.Date);      % 3M yields implied by GSW
+TTbill1.Properties.VariableNames = {'M3YLD'};
+TTbill1.Properties.DimensionNames{1} = 'Date';
+TTbill1 = TTbill1(TTbill1.Date <= dateco,:);
+TTbill2 = read_crsp();                                                          % 3M yields from CRSP
+TTbill  = [TTbill1; TTbill2];                                                   % concatenate 3M yields
+TTusyc  = synchronize(TTbill,TTgsw);                                            % merge yields (old-new)
+TTusyc  = TTusyc(TTusyc.Date < date2,:);                                        % end of sample
+datesdy = TTusyc.Date;
+datesmh = unique(lbusdate(year(datesdy),month(datesdy)));                       % end-of-month dates
+datesxc = datenum({'31-Dec-1992','30-Apr-1993','28-May-1999'});
+datesmh(ismember(datesmh,datesxc)) = datesxc - 1;                               % deal w/ special dates
+TTusyc  = TTusyc(ismember(datenum(datesdy),datesmh),:);                         % end-of-month dataset
+
+% Surveys
+ttsvy   = ttsvy(ttsvy.Date < date2,:);                                         	% end of sample
+datesqt = ttsvy.Date;
+datesqt = unique(lbusdate(year(datesqt),month(datesqt)));                       % end-of-month dates
+datesqt(ismember(datesqt,datesxc(end))) = datesxc(end) - 1;                     % deal w/ special date
+ttsvy.Date = datetime(datenum(datesqt),'ConvertFrom','datenum');
+
+% Merged dataset
+TTycsv = synchronize(TTusyc,ttsvy);                                             % merge yields and surveys
+% writetimetable(TTycsv,fullfile(pathd{2},'USYCSVYdata.xlsx'))
+
+% Preliminary analysis
+y = TTycsv{:,:}./100;                                                           % ylds as decimals
+ylds = y(:,1:8);
+svys = y(:,9:end);
+[mean(ylds,'omitnan'); std(ylds,'omitnan')]                                     % table 1a
+plot(ylds)                                                                      % figure 1a
+plot(svys,'*')                                                                  % figure 1b
+
+%% ATSM estimation
+addpath(genpath('jsz_code'))
+p     = 3;
+dt    = 1/12;
+matsY = [0.25 1:5 7 10];
+nobs  = size(ylds,1);
+Ip    = eye(p);
+W     = pca(ylds,'NumComponents',p);                                          % W': N*length(mats);
+
+% Fit the curve with JSZ (use yields only)
+[llks,AcP,BcP,AX,BX,kinfQ,K0P_cP,K1P_cP,sigma_e,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP, ...
+    llkP,llkQ,K0Q_X,K1Q_X,rho0_X,rho1_X,Sigma_cP] = sample_estimation_fun(W',ylds,matsY,dt,false);
+[llk,AcP,BcP,AX,BX,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP,yields_filtered,cP_filtered] = ...
+    jszLLK_KF(ylds,W',K1Q_X,kinfQ,Sigma_cP,matsY,dt,K0P_cP,K1P_cP,sigma_e);
+ylds_Q = ones(nobs,1)*AcP + cP_filtered*BcP;                    % with cP_filtered is same as yields_filtered
+
+% Initial values come from JSZ
+mu_xP = K0P_cP;     PhiP = K1P_cP + Ip;  Sgm = Sigma_cP;
+mu_xQ = K0Q_cP;     PhiQ = K1Q_cP + Ip;
+rho0  = rho0_cP*dt; lmbd0 = Sgm\(mu_xP - mu_xQ);
+rho1  = rho1_cP*dt; lmbd1 = Sgm\(PhiP - PhiQ);
+sgmY  = mean(std(ylds,'omitnan'));
+sgmS  = mean(std(svys,'omitnan'));
+par0  = [PhiP(:);Sgm(:);lmbd1(:);lmbd0(:);mu_xP(:);rho1(:);rho0;sgmY;sgmS];
+x00   = (Ip - PhiP)\mu_xP;                                        	% p*1
+P00   = reshape((eye(p^2)-kron(PhiP,PhiP))\reshape(Sgm,p^2,1),p,p);	% p*p
+if any(isnan(P00),'all') || any(isinf(P00),'all') || any(~isreal(eig(P00))) || any(eig(P00) < 0)
+    x00 = zeros(p,1);       P00 = Ip;                        	% in case the state is non-stationary
+end
+
+% Estimate parameters (use yields and surveys)
+matsS  = [0.25:0.25:1 10];                                          % maturities of surveys
+mats   = round([matsY matsS]/dt);                                   % maturities in months
+optns  = optimset('MaxFunEvals',length(par0)*500,'MaxIter',length(par0)*500);
+llkhd  = @(x)llkfn(x,y',x00,P00,mats,dt);                           % handle to include vars in workspace
+parest = fminsearch(llkhd,par0,optns);                              % estimate parameters
+[mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,mats,dt);                % get model parameters
+[loglk,~,~,~,~,xs,Ps] = Kfs(y',mu_x,mu_y,Phi,A,Q,R,x00,P00);        % smoothed state
+
+% Estimate the term premium (in percentage points)
+[PhiP,Sgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest);
+mu_xQ     = mu_xP - Sgm*lmbd0;
+PhiQ      = PhiP  - Sgm*lmbd1;
+matsY     = round(matsY/dt);                                        % maturities in months
+[AnQ,BnQ] = loadings4ylds(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);       % AnQ: 1*q, BnQ: p*q
+[AnP,BnP] = loadings4ylds(matsY,mu_xP,PhiP,Sgm,rho0,rho1,dt);
+tpyldsvy  = ones(nobs,1)*(AnQ - AnP) + xs'*(BnQ - BnP);
+tpyldsvy  = tpyldsvy*100;
+
+[Ay,By] = loadings4ylds(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
+ylds_P  = ones(nobs,1)*Ay + cP_filtered*By;                         % same cP as for yields_Q
+tpylds  = (ylds_Q - ylds_P)*100;
+
+end
+
+function TTcrsp = read_crsp()
+% READ_CRSP Read U.S. Treasury bill yields from CRSP
+%   TTcrsp: stores historical daily data in a timetable
+%%
+pathc  = pwd;
+pathd  = fullfile(pathc,'..','..','Data','Raw');                            % platform-specific file separators
+namefl = 'CRSP_TFZ_DLY_RF2.xlsx';                                       	% risk-free daily
+
+cd(pathd)
+opts  = detectImportOptions(namefl);
+opts  = setvartype(opts,opts.VariableNames(contains(opts.VariableNames,'CALDT')),'datetime');
+ttaux = readtimetable(namefl,opts);
+ttaux.Properties.DimensionNames{1} = 'Date';
+ttaux(isnat(ttaux.Date),:) = [];                                            % delete extra rows
+ttaux.TYLDA = ttaux.TDYLD*365*100;                                          % annualized percent
+TTcrsp = ttaux(ttaux.KYTREASNOX == 2000062,{'TYLDA'});
+TTcrsp.Properties.VariableNames = {'M3YLD'};
+cd(pathc)
+end
