cad549225f659a83c2787f9d7d589bc2a2992988
Pavel Solís <pavel.solis@gmail.com>
2020-06-03 00:10:50 -0400

    Use cSgm to get Hcov (used for mu_xQ & PhiQ), surveys loadings only if included, R versions

--- Codes/Analysis/atsm_params.m
+++ Codes/Analysis/atsm_params.m
@@ -9,35 +9,47 @@ function [mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,maturities,dt)
 matsY = maturities(1:8);                                        % maturities of yields
 matsS = maturities(9:end);                                      % maturities of surveys
 matsF = matsS + 3/12;                                         	% mats2 + tenor of rate in surveys
-% matsF = matsS + 3;                                              % mats2 + tenor (in months) of rate in surveys
 q1    = length(matsY);                                          % q = q1 + q2
 q2    = length(matsS);
 
-[PhiP,Sgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest);
+[PhiP,cSgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest);
 
 % Loadings for yields
-mu_xQ = mu_xP - chol(Sgm,'lower')*lmbd0;
-PhiQ  = PhiP  - chol(Sgm,'lower')*lmbd1;
-[AnQ,BnQ] = loadings(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);        % AnQ: 1*q1, BnQ: p*q1
-% [AnQ,BnQ] = loadings4ylds(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);   % AnQ: 1*q1, BnQ: p*q1
+Hcov  = cSgm*cSgm';
+mu_xQ = mu_xP - chol(Hcov,'lower')*lmbd0;
+PhiQ  = PhiP  - chol(Hcov,'lower')*lmbd1;
+% Hcov  = cSgm*cSgm';
+[AnQ,BnQ] = loadings(matsY,mu_xQ,PhiQ,Hcov,rho0,rho1,dt);      	% AnQ: 1*q1, BnQ: p*q1
 
-% Loadings for forward rates (to match survey forecasts)
-[~,~,ApE,BpE] = loadings(matsS,mu_xP,PhiP,zeros(size(Sgm)),rho0,rho1,dt);
-AnmE = -ApE./(matsF - matsS);
-BnmE = -BpE./(matsF - matsS);
+if ~isempty(matsS)
+    % Loadings for forward rates (to match survey forecasts)
+    [~,~,ApE,BpE] = loadings(matsS,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
+    AnmE = -ApE./(matsF - matsS);
+    BnmE = -BpE./(matsF - matsS);
+
+    % Loadings for yields and forward rates
+    AnQ = [AnQ,AnmE];                                               % 1*q = [1*q1 1*q2]
+    BnQ = [BnQ,BnmE];                                               % p*q = [p*q1 p*q2]
+end
+
+% Parameters in state space form
+mu_x = mu_xP;                                                   % p*1
+mu_y = AnQ';                                                    % q*1
+Phi  = PhiP;                                                    % p*p
+A    = BnQ';                                                    % q*p
+Q    = Hcov;                                                    % p*p
+if     isempty(sgmY) && isempty(sgmS)                           % q*q
+    R = zeros(q1);
+elseif isempty(sgmS)
+    R = diag(repmat(sgmY^2,q1,1));
+else
+    R = diag([repmat(sgmY^2,q1,1); repmat(sgmS^2,q2,1)]);
+end
+
+
+% matsF = matsS + 3;                                              % mats2 + tenor (in months) of rate in surveys
+% [AnQ,BnQ] = loadings4ylds(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);   % AnQ: 1*q1, BnQ: p*q1
 % [AnE,BnE] = loadings4ylds(matsS,mu_xP,PhiP,zeros(size(Sgm)),rho0,rho1,dt);
 % [AmE,BmE] = loadings4ylds(matsF,mu_xP,PhiP,zeros(size(Sgm)),rho0,rho1,dt);
 % AnmE = (matsF.*AmE - matsS.*AnE)./(matsF - matsS);
 % BnmE = (matsF.*BmE - matsS.*BnE)./(matsF - matsS);
-
-% Loadings for yields and forward rates
-AnQE = [AnQ,AnmE];                                              % 1*q = [1*q1 1*q2]
-BnQE = [BnQ,BnmE];                                              % p*q = [p*q1 p*q2]
-
-% Parameters in state space form
-mu_x = mu_xP;                                                   % p*1
-mu_y = AnQE';                                                   % q*1
-Phi  = PhiP;                                                    % p*p
-A    = BnQE';                                                   % q*p
-Q    = Sgm;                                                     % p*p
-R    = diag([repmat(sgmY^2,q1,1); repmat(sgmS^2,q2,1)]);       	% q*q
