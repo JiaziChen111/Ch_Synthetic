db475cf1d52ee6fb7c72871d527f8918571b9c9a
Pavel Solís <pavel.solis@gmail.com>
2020-06-03 00:01:35 -0400

    Report cSgm instead of Sgm; fix p and allow ncons to vary (the opposite as before) (cluttered version)

--- Codes/Analysis/parest2vars.m
+++ Codes/Analysis/parest2vars.m
@@ -1,13 +1,16 @@
-function [PhiP,Sgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest)
+function [PhiP,cSgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest)
 % PAREST2VARS Transform vector parest into variables contained in it
-% Parameters vectorized in parest: PhiP;Sgm;lmbd1;lmbd0;mu_xP;rho1;rho0;sgmY;sgmS
+% Parameters vectorized in parest: PhiP;cSgm;lmbd1;lmbd0;mu_xP;rho1;rho0;sgmY;sgmS
 
 % Pavel Solís (pavel.solis@gmail.com), May 2020
 %%
 % Identify dimension of state vector
-ncons = 3;                                                  % number of constants in parest
-tot   = length(parest);
-p     = (-3 + sqrt(9 - 12*(ncons - tot)))/6;                % #states (quadratic formula) given #parameters
+% ncons = 3;                                                  % number of constants in parest
+% tot   = length(parest);
+% p     = (-3 + sqrt(9 - 12*(ncons - tot)))/6;                % #states (quadratic formula) given #parameters
+
+p     = 3;
+ncons = length(parest) - 3*p*(p+1);                         % 3 matrices, 3 vectors, the rest are constants
 
 % Reshape to get constants, vectors and matrices
 aux  = reshape(parest,ncons,[]);                            % rows = #constants, appropriate #columns
@@ -18,11 +21,13 @@ matx = reshape(aux(:,1:end-1),p,p,3);                       % 3 matrices in pare
 
 % Recover variables
 PhiP  = matx(:,:,1);
-Sgm   = matx(:,:,2);
+cSgm  = matx(:,:,2);
 lmbd1 = matx(:,:,3);
 lmbd0 = vecs(:,1);
 mu_xP = vecs(:,2);
 rho1  = vecs(:,3);
 rho0  = cons(1);
-sgmY  = cons(2);
-sgmS  = cons(3);
+sgmY  = []; 
+sgmS  = [];
+if ncons > 1;   sgmY = cons(2);     end
+if ncons > 2;   sgmS = cons(3);     end
