55f826148194f47f58aacb7b1c6b73b7fba3eaa9
Pavel Solís <pavel.solis@gmail.com>
2020-06-04 11:14:27 -0400

    Includes subfucntion that generates tables A1 and A3 in the appendix (TPs JSZ and Svy)

--- Codes/Analysis/usyc_analysis.m
+++ Codes/Analysis/usyc_analysis.m
@@ -13,11 +13,21 @@ y     = y(:,2:end)./100;                                                        
 ylds  = y(:,1:8);                                                               % yield data
 svys  = y(:,9:end);                                                             % survey data
 
-% Preliminary analysis
+%% Preliminary analysis
 % table1 = [mean(ylds,'omitnan'); std(ylds,'omitnan')];                        	% table 1a
 % plot(ylds)                                                                    % figure 1a
 % plot(dates,svys,'*')                                                          % figure 1b
 % [tableA1,tableA3] = tpunrestricted();
+[tableA1,tableA3,smplsdate,smplstpjsz,smplstpsvy] = tpsurveys();
+figure                                                              % unrestricted
+plot(smplsdate{1},smplstpjsz{1}(:,end),smplsdate{2},smplstpjsz{2}(:,end),smplsdate{3},smplstpjsz{3}(:,end),...
+     smplsdate{4},smplstpjsz{4}(:,end),smplsdate{5},smplstpjsz{5}(:,end),smplsdate{6},smplstpjsz{6}(:,end))
+datetick('x','YY')
+
+figure                                                              % surveys
+plot(smplsdate{1},smplstpsvy{1}(:,end),smplsdate{2},smplstpsvy{2}(:,end),smplsdate{3},smplstpsvy{3}(:,end),...
+     smplsdate{4},smplstpsvy{4}(:,end),smplsdate{5},smplstpsvy{5}(:,end),smplsdate{6},smplstpsvy{6}(:,end))
+datetick('x','YY')
 
 %% ATSM estimation: Yields only
 addpath(genpath('jsz_code'))
@@ -37,7 +47,7 @@ ylds_Qjsz = ones(nobs,1)*AcP + cP_filtered*BcP;                             % sa
 
 %% ATSM estimation: Yields and surveys
 exitflag = 0;
-niter    = 1000;
+niter    = 2000;
 
 tic
 while exitflag == 0
@@ -122,6 +132,106 @@ figure; plot(dates(240:end),ylds_P(240:end,end),dates(240:end),ylds_Pjsz(240:end
 % [Ay,By] = loadings4ylds(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
 % [AQ,BQ] = loadings4ylds(round(matsY/dt),K0Q_cP,K1Q_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
 % [AP,BP] = loadings4ylds(round(matsY/dt),K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
+
+end
+
+
+function [tableA1,tableA3,smplsdate,smplstpjsz,smplstpsvy] = tpsurveys()
+% Load data
+% addpath(genpath('jsz_code'))
+y = readmatrix(fullfile(fullfile(pwd,'..','..','Data','Aux'),'USYCSVYdata.xlsx'),'Sheet',1);
+y(:,1)     = x2mdate(y(:,1),0);                                 % Excel to Matlab dates
+y(:,2:end) = y(:,2:end)./100;                                   % ylds as decimals
+
+% Samples
+years      = 1972:5:1997;
+nsmpls     = length(years);
+smplsdate  = cell(1,nsmpls);    smplsylds = cell(1,nsmpls);
+smplstpsvy = cell(1,nsmpls);    smplstpjsz = cell(1,nsmpls);
+tableA3    = nan(2,nsmpls);
+for k1 = 1:nsmpls
+    smplsdate{k1} = y(y(:,1) > datenum(['1-Jan-' num2str(years(k1))]),1);
+    smplsylds{k1} = y(y(:,1) > datenum(['1-Jan-' num2str(years(k1))]),2:end);
+end
+
+p      = 3;                                                     % number of factors
+dt     = 1/12;                                                  % time period in years
+matsY  = [0.25 1:5 7 10];
+matsS  = [0.25:0.25:1 10];                                      % survey maturities in years
+mats   = [matsY matsS];
+Ip     = eye(p);
+
+for k0 = 1:nsmpls
+    y    = smplsylds{k0};
+    ylds = y(:,1:8);
+    nobs = size(ylds,1);
+    W    = pca(ylds,'NumComponents',p);                         % W': N*length(mats);
+    
+    % Find initial values for the parameters (use yields only)
+    [llks,AcP,BcP,AX,BX,kinfQ,K0P_cP,K1P_cP,sigma_e,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP, ...
+        llkP,llkQ,K0Q_X,K1Q_X,rho0_X,rho1_X,Sigma_cP] = sample_estimation_fun(W',ylds,matsY,dt,false);
+    [llk,AcP,BcP,AX,BX,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP,yields_filtered,cP_filtered] = ...
+        jszLLK_KF(ylds,W',K1Q_X,kinfQ,Sigma_cP,matsY,dt,K0P_cP,K1P_cP,sigma_e);
+    ylds_Qjsz = ones(nobs,1)*AcP + cP_filtered*BcP;                      	% same as yields_filtered
+    
+    exitflag = 0;
+    niter    = 2000;
+    tic
+    while exitflag == 0
+        % Initial values from JSZ
+        mu_xP = K0P_cP;     PhiP = K1P_cP + Ip;	Hcov = Sigma_cP;
+        mu_xQ = K0Q_cP;     PhiQ = K1Q_cP + Ip;	cSgm = chol(Hcov,'lower');
+        rho0  = rho0_cP*dt;                     rho1  = rho1_cP*dt;       	% rho0 & rho1 in per period units
+        lmbd0 = cSgm\(mu_xP - mu_xQ);           lmbd1 = cSgm\(PhiP - PhiQ);	% implied lambda0 & lambda1
+        sgmY  = sigma_e;                        sgmS  = sigma_e;
+        par0  = [PhiP(:);cSgm(:);lmbd1(:);lmbd0(:);mu_xP(:);rho1(:);rho0;sgmY;sgmS];% stack model parameters
+        x00   = (Ip - PhiP)\mu_xP;                                         	% p*1
+        P00   = reshape((eye(p^2)-kron(PhiP,PhiP))\reshape(Hcov,p^2,1),p,p);% p*p
+        if any(isnan(P00),'all') || any(isinf(P00),'all') || any(~isreal(eig(P00))) || any(eig(P00) < 0)
+            x00 = zeros(p,1);       P00 = Ip;                             	% in case state is non-stationary
+        end
+        
+        % Estimate parameters (use yields and surveys)
+        maxitr = length(par0)*niter;
+        optns  = optimset('MaxFunEvals',maxitr,'MaxIter',maxitr);
+        llkhd  = @(x)llkfn(x,y',x00,P00,mats,dt);                         	% include vars in workspace
+        [parest,fval,exitflag] = fminsearch(llkhd,par0,optns);             	% estimate parameters
+        if ~isinf(fval) && exitflag == 0;   niter = niter + 1000;   end
+    end
+    toc
+    
+    % Estimate state vector based on estimated parameters
+    [mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,mats,dt);                   	% get model parameters
+    [loglk,~,~,~,~,xs]    = Kfs(y',mu_x,mu_y,Phi,A,Q,R,x00,P00);          	% smoothed state
+
+    % Estimate the term premium (in percent)
+    mu_xP = mu_x;                                                        	% p*1
+    AnQ   = mu_y(1:length(matsY))';                                       	% AnQ: 1*q
+    PhiP  = Phi;                                                        	% p*p
+    BnQ   = A(length(matsY),:)';                                           	% BnQ: p*q
+    Hcov  = Q;                                                              % p*p
+    [~,~,~,~,~,rho1,rho0] = parest2vars(parest);
+    [AnP,BnP] = loadings(matsY,mu_xP,PhiP,Hcov,rho0,rho1,dt);
+    ylds_P    = ones(nobs,1)*AnP + xs'*BnP;
+    ylds_Q    = ones(nobs,1)*AnQ + xs'*BnQ;
+    tpyldsvy  = (ylds_Q - ylds_P)*100;               % equivalent: ones(nobs,1)*(AnQ - AnP) + xs'*(BnQ - BnP);
+    smplstpsvy{k0} = tpyldsvy;
+    
+    [Ay,By]   = loadings(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
+    ylds_Pjsz = ones(nobs,1)*Ay + cP_filtered*By;                               % same cP as for yields_Q
+    tpyldjsz  = (ylds_Qjsz - ylds_Pjsz)*100;
+    smplstpjsz{k0} = tpyldjsz;
+    
+    % Mean absolute error (table A1)
+    if k0 == 1
+        maejsz = abs(ylds - ylds_Qjsz)*10000;                         % in basis points
+        maesvy = abs(ylds - ylds_Q)*10000;
+        tableA1 = [mean(maejsz); std(maejsz); mean(maesvy); std(maesvy)];
+    end
+    
+    % Asymptotic mean under P-dynamics
+    tableA3(:,k0) = [mean(mean(ylds_Pjsz))*100; mean(mean(ylds_P))*100];
+end
 
 end
 
