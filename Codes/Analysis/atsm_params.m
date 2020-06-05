7841ebdfcdb69568d9f6cb9f220005ace3a7127a
Pavel Solís <pavel.solis@gmail.com>
2020-06-05 15:03:05 -0400

    Fix sigma for surveys, use yield instead of log-price loadings, redefine forward rates

--- Codes/Analysis/atsm_params.m
+++ Codes/Analysis/atsm_params.m
@@ -8,7 +8,6 @@ function [mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,maturities,dt)
 % Identify maturities of yields and surveys
 matsY = maturities(1:8);                                        % maturities of yields
 matsS = maturities(9:end);                                      % maturities of surveys
-matsF = matsS + 3/12;                                         	% mats2 + tenor of rate in surveys
 q1    = length(matsY);                                          % q = q1 + q2
 q2    = length(matsS);
 
@@ -21,18 +20,20 @@ PhiQ  = PhiP  - chol(Hcov,'lower')*lmbd1;
 [AnQ,BnQ] = loadings(matsY,mu_xQ,PhiQ,Hcov,rho0,rho1,dt);      	% AnQ: 1*q1, BnQ: p*q1
 
 if ~isempty(matsS)
-    % Loadings for forward rates to match survey forecasts (use price loadings)
-    [~,~,ApE,BpE] = loadings(matsS,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
-    AnmE = -ApE./matsS;
-    BnmE = -BpE./matsS;
-%     [~,~,ApS,BpS] = loadings(matsS,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
-%     [~,~,ApF,BpF] = loadings(matsF,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
-%     AnmE = (-ApF - (-ApS))./(matsF - matsS);
-%     BnmE = (-BpF - (-BpS))./(matsF - matsS);
+    % Loadings for survey yields
+    [AnS,BnS] = loadings(matsS,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
     
-    % Loadings for yields and forward rates
-    AnQ = [AnQ,AnmE];                                           % 1*q = [1*q1 1*q2]
-    BnQ = [BnQ,BnmE];                                        	% p*q = [p*q1 p*q2]
+    % Loadings for forward rates to match survey forecasts (use log-price loadings)
+    % matsN = maturities(9:end);                             	% define starting date of forward rate
+    % matsM = matsN + 3/12;                                 	% define ending date of forward rate
+    % [~,~,ApN,BpN] = loadings(matsN,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
+    % [~,~,ApM,BpM] = loadings(matsM,mu_xP,PhiP,zeros(size(Hcov)),rho0,rho1,dt);
+    % AnS = (-ApM - (-ApN))./(matsM - matsN);
+    % BnS = (-BpM - (-BpN))./(matsM - matsN);
+    
+    % Loadings for bond yields and survey yields
+    AnQ = [AnQ,AnS];                                            % 1*q = [1*q1 1*q2]
+    BnQ = [BnQ,BnS];                                        	% p*q = [p*q1 p*q2]
 end
 
 % Parameters in state space form
@@ -42,9 +43,10 @@ Phi  = PhiP;                                                    % p*p
 A    = BnQ';                                                    % q*p
 Q    = Hcov;                                                    % p*p
 if     isempty(sgmY) && isempty(sgmS)                           % q*q
-    R = zeros(q1);
+    R = zeros(q1);                                              % this case causes problems
 elseif isempty(sgmS)
-    R = diag(repmat(sgmY^2,q1,1));
+    R = diag([repmat(sgmY^2,q1,1); repmat(0.0075^2,q2,1)]);     % fixed sgmS case
+    % R = diag(repmat(sgmY^2,q1,1));                            % yields only case
 else
     R = diag([repmat(sgmY^2,q1,1); repmat(sgmS^2,q2,1)]);
 end
