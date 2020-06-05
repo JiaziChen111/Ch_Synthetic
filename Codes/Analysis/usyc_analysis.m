d588fd7ae13e454f5096ca9cca45b10e1fbd9020
Pavel Solís <pavel.solis@gmail.com>
2020-06-05 17:16:59 -0400

    Reorganize and clean code

--- Codes/Analysis/usyc_analysis.m
+++ Codes/Analysis/usyc_analysis.m
@@ -1,33 +1,20 @@
 function usyc_analysis()
-% USYC_ANALYSIS Reproduce analysis of Guimaraes (2014)
+% USYC_ANALYSIS Reproduce analysis in Guimaraes (2014)
 
-% m-files called: y_NSS, Kfs
+% m-files called: y_NSS, atsm_params, Kfs
 % Pavel Solís (pavel.solis@gmail.com), May 2020
 % 
 %% Load data
 % TTycsv = usycsvy_data ();
 % y   = TTycsv{:,:}./100;
 y     = readmatrix(fullfile(fullfile(pwd,'..','..','Data','Aux','USYCSVY'),'USYCSVYdata.xlsx'),'Sheet',1);
-dates = x2mdate(y(:,1));                                                        % dates as datenum
-y     = y(:,2:end)./100;                                                        % data in decimals
-ylds  = y(:,1:8);                                                               % yield data
-svys  = y(:,9:end);                                                             % survey data
-
-%% Preliminary analysis
-% table1 = [mean(ylds,'omitnan'); std(ylds,'omitnan')];                        	% table 1a
-% plot(ylds)                                                                    % figure 1a
-% plot(dates,svys,'*')                                                          % figure 1b
-% [tableA1,tableA3,smplsdate,smplsylds,smplstpjsz] = tpunrestricted();
-[tableA1,tableA3,smplsdate,smplstpjsz,smplstpsvy,smplsparam,smplsxs,smplsllk] = tpsurveys();
-figure                                                              % figure 3 (3 factors): unrestricted
-plot(smplsdate{1},smplstpjsz{1}(:,end),smplsdate{2},smplstpjsz{2}(:,end),smplsdate{3},smplstpjsz{3}(:,end),...
-     smplsdate{4},smplstpjsz{4}(:,end),smplsdate{5},smplstpjsz{5}(:,end),smplsdate{6},smplstpjsz{6}(:,end))
-datetick('x','YY'); ylim([-2 8]); yline(0);
-
-figure                                                              % figure 3 (3 factors): surveys
-plot(smplsdate{1},smplstpsvy{1}(:,end),smplsdate{2},smplstpsvy{2}(:,end),smplsdate{3},smplstpsvy{3}(:,end),...
-     smplsdate{4},smplstpsvy{4}(:,end),smplsdate{5},smplstpsvy{5}(:,end),smplsdate{6},smplstpsvy{6}(:,end))
-datetick('x','YY'); ylim([-2 8]); yline(0);
+dates = x2mdate(y(:,1));                                                 	% dates as datenum
+y     = y(:,2:end)./100;                                                   	% data in decimals
+ylds  = y(:,1:8);                                                          	% yield data
+svys  = y(:,9:end);                                                        	% survey data
+% table1 = [mean(ylds,'omitnan'); std(ylds,'omitnan')];                   	% table 1a
+% plot(ylds)                                                              	% figure 1a
+% plot(dates,svys,'*')                                                  	% figure 1b
 
 %% ATSM estimation: Yields only
 addpath(genpath('jsz_code'))
@@ -38,17 +25,21 @@ nobs  = size(ylds,1);                                                       % nu
 Ip    = eye(p);
 W     = pca(ylds,'NumComponents',p);                                    	% W': N*length(mats);
 
-% Estimate parameters using JSZ normalization (use yields only)
+% Estimate parameters via JSZ normalization (use yields only: ylds)
 [llks,AcP,BcP,AX,BX,kinfQ,K0P_cP,K1P_cP,sigma_e,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP, ...
     llkP,llkQ,K0Q_X,K1Q_X,rho0_X,rho1_X,Sigma_cP] = sample_estimation_fun(W',ylds,matsY,dt,false);
 [llk,AcP,BcP,AX,BX,K0Q_cP,K1Q_cP,rho0_cP,rho1_cP,cP,yields_filtered,cP_filtered] = ...
-    jszLLK_KF(ylds,W',K1Q_X,kinfQ,Sigma_cP,matsY,dt,K0P_cP,K1P_cP,sigma_e); % 
+    jszLLK_KF(ylds,W',K1Q_X,kinfQ,Sigma_cP,matsY,dt,K0P_cP,K1P_cP,sigma_e);
 ylds_Qjsz = ones(nobs,1)*AcP + cP_filtered*BcP;                             % same as yields_filtered
+[Ay,By]   = loadings(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
+ylds_Pjsz = ones(nobs,1)*Ay + cP_filtered*By;                               % same cP as for yields_Q
+tpyldjsz  = (ylds_Qjsz - ylds_Pjsz)*100;                                    % in percent
 
 %% ATSM estimation: Yields and surveys
+matsS    = [0.25:0.25:1 10];                                                % survey maturities in years
+mats     = [matsY matsS];
 exitflag = 0;
 niter    = 2000;
-
 tic
 while exitflag == 0
     if niter == 2000
@@ -58,24 +49,20 @@ while exitflag == 0
         rho0  = rho0_cP*dt;                     rho1  = rho1_cP*dt;      	% rho0 & rho1 in per period units
         lmbd0 = cSgm\(mu_xP - mu_xQ);           lmbd1 = cSgm\(PhiP - PhiQ);	% implied lambda0 & lambda1
         sgmY  = sigma_e;                        sgmS  = sigma_e;
-        par0  = [PhiP(:);cSgm(:);lmbd1(:);lmbd0(:);mu_xP(:);rho1(:);rho0;sgmY;sgmS];% stack model parameters
+        %par0  = [PhiP(:);cSgm(:);lmbd1(:);lmbd0(:);mu_xP(:);rho1(:);rho0;sgmY;sgmS];% stack model parameters
+        par0  = [PhiP(:);cSgm(:);lmbd1(:);lmbd0(:);mu_xP(:);rho1(:);rho0;sgmY]; % fixed sgmS case
         x00   = (Ip - PhiP)\mu_xP;                                      	% p*1
         P00   = reshape((eye(p^2)-kron(PhiP,PhiP))\reshape(Hcov,p^2,1),p,p);% p*p
         if any(isnan(P00),'all') || any(isinf(P00),'all') || any(~isreal(eig(P00))) || any(eig(P00) < 0)
             x00 = zeros(p,1);       P00 = Ip;                               % in case state is non-stationary
         end
-
-        % Check initial values
-        matsS  = [0.25:0.25:1 10];                                        	% survey maturities in years
-        mats   = [matsY matsS];
-        [mu_x0,mu_y0,Phi0,A0,Q0,R0] = atsm_params(par0,mats,dt);
+        [mu_x0,mu_y0,Phi0,A0,Q0,R0] = atsm_params(par0,mats,dt);            % check initial values
         [loglk0,~,~,~,~,xs0]        = Kfs(y',mu_x0,mu_y0,Phi0,A0,Q0,R0,x00,P00);
-        % plot(dates,cP_filtered(:,1),dates,xs0(1,:))
     else
         par0 = parest;
     end
-
-    % Estimate parameters (use yields and surveys)
+    
+    % Estimate parameters (use yields and surveys: y)
     maxitr = length(par0)*niter;
     optns  = optimset('MaxFunEvals',maxitr,'MaxIter',maxitr);
     llkhd  = @(x)llkfn(x,y',x00,P00,mats,dt);                             	% include vars in workspace
@@ -88,7 +75,7 @@ toc
 [mu_x,mu_y,Phi,A,Q,R] = atsm_params(parest,mats,dt);                        % get model parameters
 [loglk,~,~,~,~,xs]    = Kfs(y',mu_x,mu_y,Phi,A,Q,R,x00,P00);                % smoothed state
 
-% Estimate the term premium (in percent)
+% Estimate the term premium
 mu_xP = mu_x;                                                               % p*1
 AnQ   = mu_y(1:length(matsY))';                                             % AnQ: 1*q
 PhiP  = Phi;                                                                % p*p
@@ -98,44 +85,28 @@ Hcov  = Q;                                                                  % p*
 [AnP,BnP] = loadings(matsY,mu_xP,PhiP,Hcov,rho0,rho1,dt);
 ylds_P    = ones(nobs,1)*AnP + xs'*BnP;
 ylds_Q    = ones(nobs,1)*AnQ + xs'*BnQ;
-tpyldsvy  = (ylds_Q - ylds_P)*100;                	% equivalent: ones(nobs,1)*(AnQ - AnP) + xs'*(BnQ - BnP);
-
-[Ay,By]   = loadings(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
-ylds_Pjsz = ones(nobs,1)*Ay + cP_filtered*By;                               % same cP as for yields_Q
-tpyldjsz  = (ylds_Qjsz - ylds_Pjsz)*100;
+tpyldsvy  = (ylds_Q - ylds_P)*100;                              % = ones(nobs,1)*(AnQ - AnP) + xs'*(BnQ - BnP);
 
 % Figures
+figure; plot(dates,cP_filtered(:,1),dates,xs0(1,:))
 figure; plot(dates,cP_filtered(:,1),dates,xs(1,:))
 figure; plot(dates,ylds(:,end),dates,ylds_Qjsz(:,end),dates,ylds_Q(:,end))
 figure; plot(dates,tpyldsvy(:,end),dates,tpyldjsz(:,end))
 figure; plot(dates(240:end),ylds_P(240:end,end),dates(240:end),ylds_Pjsz(240:end,end),...
              dates(240:end),svys(240:end,end),'*')
 
+%% Figure 3 & Appendix
+% [tableA1,tableA3,smplsdate,smplsylds,smplstpjsz] = tpunrestricted();
+[tableA1,tableA3,smplsdate,smplstpjsz,smplstpsvy,smplsparam,smplsxs,smplsllk] = tpsurveys();
+figure                                                                      % figure 3: unrestricted
+plot(smplsdate{1},smplstpjsz{1}(:,end),smplsdate{2},smplstpjsz{2}(:,end),smplsdate{3},smplstpjsz{3}(:,end),...
+     smplsdate{4},smplstpjsz{4}(:,end),smplsdate{5},smplstpjsz{5}(:,end),smplsdate{6},smplstpjsz{6}(:,end))
+datetick('x','YY'); ylim([-2 8]); yline(0);
 
-% sgmY  = mean(std(ylds,'omitnan'));
-% sgmS  = mean(std(svys,'omitnan'));
-% [PhiP9,cSgm9,lmbd19,lmbd09,mu_xP9,rho19,rho09,sgmY9,sgmS9] = parest2vars(par0);  % check parest2vars works
-% mats   = matsY;                                                 % maturities in years
-% [loglk0,~,~,~,~,xs0,Ps0] = Kfs(ylds',mu_x0,mu_y0,Phi0,A0,Q0,R0,x00,P00);
-% mats   = matsY;                                                 % maturities in years
-% matsS  = [0.25:0.25:1 10];                                     	% maturities of surveys
-% mats   = [matsY matsS];
-% llkhd  = @(x)llkfn(x,ylds',x00,P00,mats,dt);                  	% handle to include vars in workspace
-% parest = fminsearch(llkhd,par0,optns);                          % estimate parameters
-% [loglk,~,~,~,~,xs] = Kfs(ylds',mu_x,mu_y,Phi,A,Q,R,x00,P00);     	% smoothed state
-% [PhiP,cSgm,lmbd1,lmbd0,mu_xP,rho1,rho0] = parest2vars(parest);
-% Hcov      = cSgm*cSgm';
-% mu_xQ     = mu_xP - chol(Hcov,'lower')*lmbd0;
-% PhiQ      = PhiP  - chol(Hcov,'lower')*lmbd1;
-% [AnQ,BnQ] = loadings(matsY,mu_xQ,PhiQ,Hcov,rho0,rho1,dt);
-
-% mats   = round([matsY matsS]/dt);                                   % maturities in months
-% matsY     = round(matsY/dt);                                        % maturities in months
-% [AnQ,BnQ] = loadings4ylds(matsY,mu_xQ,PhiQ,Sgm,rho0,rho1,dt);       % AnQ: 1*q, BnQ: p*q
-% [AnP,BnP] = loadings4ylds(matsY,mu_xP,PhiP,Sgm,rho0,rho1,dt);
-% [Ay,By] = loadings4ylds(matsY,K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
-% [AQ,BQ] = loadings4ylds(round(matsY/dt),K0Q_cP,K1Q_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
-% [AP,BP] = loadings4ylds(round(matsY/dt),K0P_cP,K1P_cP+Ip,Sigma_cP,rho0_cP*dt,rho1_cP*dt,dt);
+figure                                                                      % figure 3: surveys
+plot(smplsdate{1},smplstpsvy{1}(:,end),smplsdate{2},smplstpsvy{2}(:,end),smplsdate{3},smplstpsvy{3}(:,end),...
+     smplsdate{4},smplstpsvy{4}(:,end),smplsdate{5},smplstpsvy{5}(:,end),smplsdate{6},smplstpsvy{6}(:,end))
+datetick('x','YY'); ylim([-2 8]); yline(0);
 
 end
 
