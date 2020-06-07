0532aa6799c1d1f57612cdd0e638960eec0a106e
Pavel Solís <pavel.solis@gmail.com>
2020-06-07 13:43:27 -0400

    Use balanced panel for advanced countries (only yields) so that field has data for all countries

--- Codes/Analysis/append_surveys.m
+++ Codes/Analysis/append_surveys.m
@@ -1,8 +1,10 @@
 function S = append_surveys(S,currEM)
 % APPEND_SURVEYS Append survey forecasts for the policy rate to yield data
+% of emerging markets; only yield data for advanced countries
 % 
-% Pavel Solís (pavel.solis@gmail.com), June 2019
+% Pavel Solís (pavel.solis@gmail.com), June 2020
 %%
+ncntrs = length(S);
 nEMs   = length(currEM);
 fnames = fieldnames(S);
 prefix = {'n_','s_'};
@@ -11,8 +13,8 @@ for k0 = 1:2
     for k1 = 1:nEMs
         hdry  = S(k1).(fnameb)(1,:);                                % yield maturities (include first column)
         hdrv  = S(k1).svys(1,2:end);                                % survey maturities
-        ylds  = S(k1).(fnameb)(2:end,2:end);                        % already in decimals
-        svys  = S(k1).svys(2:end,2:end)/100;                        % in decimals
+        ylds  = S(k1).(fnameb)(2:end,2:end);                        % yields already in decimals
+        svys  = S(k1).svys(2:end,2:end)/100;                        % survey forecasts in decimals
         datey = S(k1).(fnameb)(2:end,1);                            % dates of yields
         datev = S(k1).svys(2:end,1);                                % dates of surveys
         fltrd = datev >= datey(1);                                  % survey data in sample period
@@ -22,5 +24,8 @@ for k0 = 1:2
         TTs   = array2timetable(svys,'RowTimes',datetime(datev,'ConvertFrom','datenum'));
         TT    = synchronize(TTy,TTs,'union');
         S(k1).([prefix{k0} 'y']) = [hdry hdrv; datenum(TT.Time) TT{:,:}];
+    end
+    for k2 = nEMs+1:ncntrs
+        S(k2).([prefix{k0} 'y']) = S(k2).(fnameb);
     end
 end
