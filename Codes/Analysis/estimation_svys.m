f060bea3d6b6afe2feb3a81e455947b06755f66c
Pavel Solís <pavel.solis@gmail.com>
2020-06-08 17:49:50 -0400

    Add option on whether to estimate sgmS, obtain cSgm from chol not estimation, reorder params

--- Codes/Analysis/estimation_svys.m
+++ Codes/Analysis/estimation_svys.m
@@ -1,13 +1,14 @@
-function [ylds_Q,ylds_P,termprm,params] = estimation_svys(yldsvy,matsY,matsS,matsout,dt,params0)
+function [ylds_Q,ylds_P,termprm,params] = estimation_svys(yldsvy,matsY,matsS,matsout,dt,params0,sgmSfree)
 % ESTIMATION_SVYS Estimate affine term structure model using yields and surveys
 % 
 %	INPUTS
-% yldsvy  - bond yields and survey forecasts (rows: obs, cols: maturities)
-% matsY   - maturities of yields in years
-% matsS   - maturities of surveys in years
-% matsout - bond maturities in years to be reported
-% dt      - length of period in years (eg. 1/12 for monthly data)
-% params0 - initial values of parameters
+% yldsvy   - bond yields and survey forecasts (rows: obs, cols: maturities)
+% matsY    - maturities of yields in years
+% matsS    - maturities of surveys in years
+% matsout  - bond maturities in years to be reported
+% dt       - length of period in years (eg. 1/12 for monthly data)
+% params0  - initial values of parameters
+% sgmSfree - logical variable for whether to estimate sgmS
 %
 %	OUTPUT
 % ylds_Q  - estimated yields under Q measure
@@ -27,8 +28,13 @@ niter  = 2000;
 % Estimate parameters
 while fmflag == 0
     if niter == 2000                                                        % use initial values in 1st run
-        par0 = [params0.PhiP(:);params0.cSgm(:);params0.lmbd1(:);params0.lmbd0(:);...
-                params0.mu_xP(:);params0.rho1(:);params0.rho0;params0.sgmY;params0.sgmS];% sgmS fixed, defined in atsm_params
+        if sgmSfree
+            par0 = [params0.PhiP(:);params0.cSgm(:);params0.lmbd1(:);params0.lmbd0(:);...
+                    params0.mu_xP(:);params0.rho1(:);params0.rho0;params0.sgmY;params0.sgmS];
+        else
+            par0 = [params0.PhiP(:);params0.cSgm(:);params0.lmbd1(:);params0.lmbd0(:);...
+                    params0.mu_xP(:);params0.rho1(:);params0.rho0;params0.sgmY]; % sgmS fixed in atsm_params
+        end
     else
         par0 = parest;
     end
@@ -46,9 +52,8 @@ end
 
 % Estimate the term premium
 [PhiP,cSgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS] = parest2vars(parest);
-Hcov  = cSgm*cSgm';
-mu_xQ = mu_xP - cSgm*lmbd0;
-PhiQ  = PhiP  - cSgm*lmbd1;
+Hcov      = cSgm*cSgm';             cSgm = chol(Hcov,'lower');              % crucial: cSgm from Cholesky
+mu_xQ     = mu_xP - cSgm*lmbd0;     PhiQ = PhiP  - cSgm*lmbd1;
 [AnQ,BnQ] = loadings(matsout,mu_xQ,PhiQ,Hcov,rho0,rho1,dt);
 [AnP,BnP] = loadings(matsout,mu_xP,PhiP,Hcov,rho0,rho1,dt);
 ylds_Q    = ones(nobs,1)*AnQ + xs'*BnQ;
@@ -58,6 +63,7 @@ termprm   = ylds_Q - ylds_P;                                    % = ones(nobs,1)
 % Report parameters
 params.mu_xP = mu_xP;   params.PhiP  = PhiP;
 params.mu_xQ = mu_xQ;   params.PhiQ  = PhiQ;
-params.rho0  = rho0;    params.lmbd0 = lmbd0;
-params.rho1  = rho1;    params.lmbd1 = lmbd1;
-params.cSgm  = cSgm;    params.sgmY  = sgmY;    params.sgmS  = sgmS;        % sgmS reported for completeness
+params.rho0  = rho0;    params.rho1  = rho1;
+params.lmbd0 = lmbd0;   params.lmbd1 = lmbd1;
+params.sgmY  = sgmY;    params.sgmS  = sgmS;
+params.cSgm  = cSgm;    params.xs    = xs;
