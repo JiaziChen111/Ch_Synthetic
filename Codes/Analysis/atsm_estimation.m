7402966d3e5d149b1f68079536af8fe41e93bcc6
Pavel Solís <pavel.solis@gmail.com>
2020-06-09 18:40:24 -0400

    sgmS added as an input, automatically adjust field name depending on sgmS value

--- Codes/Analysis/atsm_estimation.m
+++ Codes/Analysis/atsm_estimation.m
@@ -1,11 +1,12 @@
-function S = atsm_estimation(S,matsout)
+function S = atsm_estimation(S,matsout,sgmSfree)
 % ATSM_ESTIMATION Estimate affine term structure model with monthly data
 % and 3 pricing factors
 % 
 %	INPUTS
-% S       - structure with fields n_y and s_y containing nominal and 
-%           synthetic bond yields (and survey forecasts)
-% matsout - bond maturities in years to be reported
+% S        - structure with fields n_y and s_y containing nominal and 
+%            synthetic bond yields (and survey forecasts)
+% matsout  - bond maturities in years to be reported
+% sgmSfree - logical variable for whether to estimate sgmS
 %
 %	OUTPUT
 % S - structure includes estimated yields under Q and P measures, estimated
@@ -20,6 +21,7 @@ dt      = 1/12;                                                          	% mont
 ncntrs  = length(S);
 fnames  = fieldnames(S);
 prefix  = {'n','s'};
+if sgmSfree; sgmtype = 'f'; else; sgmtype = 'b'; end                        % free vs baseline case
 
 for k0 = 1:2
     fldname = fnames{contains(fnames,[prefix{k0} '_ylds'])};
@@ -49,12 +51,12 @@ for k0 = 1:2
         
         % Estimate the model using yields and surveys
         if ~isempty(matsS)                                                  % only for EMs w/ survey data
-            [ylds_Q,ylds_P,termprm,params] = estimation_svys(ynsvys,matsY,matsS,matsout,dt,params0,false);
+            [ylds_Q,ylds_P,termprm,params] = estimation_svys(ynsvys,matsY,matsS,matsout,dt,params0,sgmSfree);
             
-            S(k1).([prefix{k0} 'sb_yQ']) = [nan matsout; dates ylds_Q];
-            S(k1).([prefix{k0} 'sb_yP']) = [nan matsout; dates ylds_P];
-            S(k1).([prefix{k0} 'sb_tp']) = [nan matsout; dates termprm];
-            S(k1).([prefix{k0} 'sb_pr']) = params;
+            S(k1).([prefix{k0} 's' sgmtype '_yQ']) = [nan matsout; dates ylds_Q];
+            S(k1).([prefix{k0} 's' sgmtype '_yP']) = [nan matsout; dates ylds_P];
+            S(k1).([prefix{k0} 's' sgmtype '_tp']) = [nan matsout; dates termprm];
+            S(k1).([prefix{k0} 's' sgmtype '_pr']) = params;
         end
         disp(['Estimation for ' S(k1).cty ' has finished.'])
     end
