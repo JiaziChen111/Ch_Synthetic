b89fb772c862bcc4dba7105f9ff5efbbba0b888a
Pavel Solís <pavel.solis@gmail.com>
2020-06-25 19:58:04 -0400

    Report dates of balanced panels

---
+++ Codes/Analysis/datesminmax.m
@@ -0,0 +1,10 @@
+function [dtmn,dtmx] = datesminmax(S,k0)
+% DATESMINMAX Return the first dates of the balanced panels for nominal
+% and synthetic yields
+
+% Pavel Solís (pavel.solis@gmail.com), June 2020
+%%
+date1 = datenum(S(k0).n_dateb,'mmm-yyyy');
+date2 = datenum(S(k0).s_dateb,'mmm-yyyy');
+dtmn  = min(date1,date2); 
+dtmx  = max(date1,date2);
