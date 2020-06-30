9582b3dc4adc53f0c7b42e6ff9103ad253fa59e8
Pavel Solís <pavel.solis@gmail.com>
2020-06-30 14:44:30 -0400

    Script turned into a function, stores fitted yields and estimated tp in S

--- Codes/Analysis/atsm_daily.m
+++ Codes/Analysis/atsm_daily.m
@@ -1,10 +1,26 @@
+function S = atsm_daily(S,matsout,currEM,plotfit)
+% ATSM_DAILY Estimate affine term structure model with daily data
+% 
+%	INPUTS
+% S        - structure with fields bsl_pr, s, n, ds, dn
+% matsout  - bond maturities in years to be reported
+% currEM   - emerging market countries
+% plotfit  - logical indicating whether to show a plot of the fit
+%
+%	OUTPUT
+% S - structure includes estimated yields under Q and P measures, estimated term premia
+
+% m-files called: none
+% Pavel Solís (pavel.solis@gmail.com), June 2020
+%%
 ncntrs  = length(S);
 nEMs    = length(currEM);
 matsall = [0.25 0.5 1:10];                                      % all possible maturities
 mtxmae  = nan(ncntrs,length(matsall));  mtxsae = nan(ncntrs,length(matsall)); %fit using absolute errors
 fnames  = fieldnames(S);
 fnameq  = fnames{contains(fnames,'bsl_pr')};                    % field containing estimated parameters
-p       = 3;                                                    % number of pricing factors
+dt      = 1/12;                                                 % period length in years
+prefix  = 'd';                                                  % prefix for fields storing the results
 intctrs = setdiff(currEM,{'ILS','ZAR'});                        % countries w/ intercept
 
 for k0 = 1:ncntrs
@@ -18,16 +34,14 @@ for k0 = 1:ncntrs
     % Monthly data
     fldname = fnames{contains(fnames,[prfxM '_blncd'])};        % field for monthly data
     mats    = S(k0).(fldname)(1,2:end);                       	% original maturities
-    datesM  = S(k0).(fldname)(2:end,1);                         % monthly dates
     yieldsM = S(k0).(fldname)(2:end,2:end);                     % monthly yields
     nobsM   = size(yieldsM,1);                                  % #monthly observations
-    muM     = mean(yieldsM);                                    % average of monthly yields per maturity
     
     % Smoothed state
     xsM = S(k0).(fnameq).xs;
     if size(xsM,2) == nobsM; xsM = xsM'; end                    % ensure xs dimensions are the same as yields
     
-    % Implied weights from smoothed state using least-squares
+    % Implied monthly weights from smoothed state using least squares
     if ~ismember(S(k0).iso,intctrs)
         Wm   = (yieldsM'*yieldsM)\yieldsM'*xsM;                	% no intercept
     else
@@ -37,40 +51,49 @@ for k0 = 1:ncntrs
         Wm   = Wmls(2:end,:);
     end
     
-    % Daily pricing factors
+    % Estimate daily pricing factors using monthly weights
     fldname = fnames{contains(fnames,[prfxD '_blncd'])};        % field for daily data
     datesD  = S(k0).(fldname)(2:end,1);                        	% daily dates
     yieldsD = S(k0).(fldname)(2:end,2:end);                     % daily yields
     nobsD   = size(yieldsD,1);                                	% #daily observations
     if ~ismember(S(k0).iso,intctrs)
-        xsD2 = yieldsD*Wm;
+        xsD = yieldsD*Wm;
     else
-        xsD2 = repmat(Wmc,nobsD,1) + yieldsD*Wm;
+        xsD = repmat(Wmc,nobsD,1) + yieldsD*Wm;
     end
     
-    % Fitted yields
+    % Fit yields and estimate the term premium
     cSgm  = S(k0).(fnameq).cSgm;    Hcov  = cSgm*cSgm';       	% estimated parameters
     mu_xQ = S(k0).(fnameq).mu_xQ;   PhiQ  = S(k0).(fnameq).PhiQ;
+    mu_xP = S(k0).(fnameq).mu_xP;   PhiP  = S(k0).(fnameq).PhiP;
     rho0  = S(k0).(fnameq).rho0;    rho1  = S(k0).(fnameq).rho1;
-    [AnQ,BnQ] = loadings(mats,mu_xQ,PhiQ,Hcov,rho0,rho1,1/12);	% loadings using original maturities 
-    yieldsQ   = (ones(nobsD,1)*AnQ + xsD2*BnQ)*100;          	% fitted daily yields in percent
-    yieldsD   = yieldsD*100;                                    % observed daily yields in percent
+    [AnQ,BnQ] = loadings(mats,mu_xQ,PhiQ,Hcov,rho0,rho1,dt);	% loadings using original maturities 
+    [AnP,BnP] = loadings(mats,mu_xP,PhiP,Hcov,rho0,rho1,dt);
+    ylds_Q    = ones(nobsD,1)*AnQ + xsD*BnQ;                    % fitted daily yields in percent
+    ylds_P    = ones(nobsD,1)*AnP + xsD*BnP;                    % risk neutral yields
+    termprm   = ylds_Q - ylds_P;                                % in decimals
     
     % Assess fit of the model
-    mtxmae(k0,ismember(matsall,mats)) = mean(abs(yieldsD - yieldsQ))*100;   % mean absolute errors in bp
-    mtxsae(k0,ismember(matsall,mats)) = std(abs(yieldsD - yieldsQ))*100;    % std of absolute errors in bp
-    S(k0).('db_rmse') = sqrt(mean(mean((yieldsD - yieldsQ).^2)));           % RMSE
+    mtxmae(k0,ismember(matsall,mats)) = mean(abs(yieldsD - ylds_Q))*10000;  % mean absolute errors in bp
+    mtxsae(k0,ismember(matsall,mats)) = std(abs(yieldsD - ylds_Q))*10000;   % std of absolute errors in bp
+    
+    % Store variables
+    fltr = ismember(mats,matsout);
+    S(k0).([prefix '_yQ']) = [nan matsout; datesD ylds_Q(:,fltr)];
+    S(k0).([prefix '_yP']) = [nan matsout; datesD ylds_P(:,fltr)];
+    S(k0).([prefix '_tp']) = [nan matsout; datesD termprm(:,fltr)];
+    S(k0).([prefix '_rmse']) = sqrt(mean(mean(10000*(yieldsD - ylds_Q).^2)));% RMSE
     
     if plotfit
         if ismember(S(k0).iso,currEM)
             if k0 == 1; figure; end
             subplot(3,5,k0);
-            plot(datesD,yieldsD(:,end),datesD,yieldsQ(:,end))
+            plot(datesD,yieldsD(:,end),datesD,ylds_Q(:,end))
             title(S(k0).cty); datetick('x','yy'); yline(0); ylabel('%');
         else
             if k0 == nEMs+1; figure; end
             subplot(2,5,k0-nEMs);
-            plot(datesD,yieldsD(:,end),datesD,yieldsQ(:,end))
+            plot(datesD,yieldsD(:,end),datesD,ylds_Q(:,end))
             title(S(k0).cty); datetick('x','yy'); yline(0); ylabel('%');
         end 
     end
