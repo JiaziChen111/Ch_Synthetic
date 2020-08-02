c577b1eb298f9bb9ea183c89532133163b098ada
Pavel Solís <pavel.solis@gmail.com>
2020-08-02 15:03:02 -0400

    Relocate code for ATSM of US YC, delete old code (comparing TPnom/TPsyn, AE/EM TP, term spread)

--- Codes/Analysis/ts_analysis.m
+++ Codes/Analysis/ts_analysis.m
@@ -115,150 +115,46 @@ TT = construct_panel(S,matsout,currEM,currAE);
 % notradedays = TTusyc.Date(sum(ismissing(TTusyc),2) == size(TTusyc,2));
 % tradingdays = TTusyc.Date(~ismember(TTusyc.Date,notradedays));      % trading days in the U.S.
 
-%% US TP
-ynsvys = readmatrix(fullfile(fullfile(pwd,'..','..','Data','Aux','USYCSVY'),...
-    'USYCSVYdata.xlsx'),'Sheet',1);
-dates  = x2mdate(ynsvys(:,1));                                  	% dates as datenum
-ynsvys = ynsvys(:,2:end)./100;                                   	% data in decimals
-yonly  = ynsvys(:,1:8);                                          	% yield data
-matsY  = [0.25 1:5 7 10];                                           % yield maturities in years
-matsS  = [0.25:0.25:1 10];
-p      = 3;                                                       	% number of state vectors
-dt     = 1/12;                                                     	% monthly periods
-matout = [1 5 10];
 
-[ylds_Qjsz,ylds_Pjsz,tpjsz,params0] = estimation_jsz(yonly,matsY,matout,dt,p);
-[ylds_Q,ylds_P,termprm,params] = estimation_svys(ynsvys,matsY,matsS,matout,dt,params0);
-
-figure; plot(dates,yonly(:,end),dates,ylds_Qjsz(:,end),dates,ylds_Q(:,end))
-figure; plot(dates,termprm(:,end),dates,tpjsz(:,end))
-svys  = ynsvys(:,9:end);
-figure; plot(dates(240:end),ylds_P(240:end,end),dates(240:end),ylds_Pjsz(240:end,end),...
-             dates(240:end),svys(240:end,end),'*')
-
-
-%% Decomposition of Nominal Yield Curves and TP Comparison
-
-ncntrs  = length(S);
-tnr = 10;
-decomp = nan(ncntrs,5);
-tpvslccs = nan(ncntrs,3);
-tpcompare = nan(ncntrs,2);
-tpnomvssyn = nan(ncntrs,3);
-
-for k = 1:ncntrs
-    yieldsnom = mean(S(k).nomblncd(2:end, S(k).nomblncd(1,:) == tnr)*100);
-    yieldssyn = mean(S(k).synblncd(2:end, S(k).synblncd(1,:) == tnr)*100);
-    yieldsP   = mean(S(k).synyldsP(2:end, S(k).synyldsP(1,:) == tnr)*100);
-    x = S(k).syntp(3:end, S(k).syntp(1,:) == tnr);
-    tpsyn     = mean(x);
-    y = S(k).cipdev(2:end, S(k).cipdev(1,:) == tnr);
-    lccs      = mean(y,'omitnan');
-    decomp(k,:) = [yieldsnom yieldssyn yieldsP tpsyn lccs];
-    [h,p] = ttest2(x,y,'Vartype','unequal');     % Equality of means test b/w TP and LCCS
-    tpvslccs(k,:) = [k h p];
-    
-    z = S(k).nomtp(3:end, S(k).nomtp(1,:) == tnr);
-    tpnom     = mean(z);
-    tpcompare(k,:) = [tpnom tpsyn];
-    [h,p] = ttest2(z,x,'Vartype','unequal');     % Equality of means test b/w TP and LCCS
-    tpnomvssyn(k,:) = [k h p];
-end
-
-AvgDecomp = [mean(decomp(1:15,:)); mean(decomp([16:19 23:25],:)); mean(decomp(20:22,:))];
-clear input
-labelcty    = {'EM','A-SOE','G-3'};
-labelcty{1} = [' ' labelcty{1}];                % Otherwise, Latex gives an error
-input.tableRowLabels = labelcty;
-input.tableColLabels = {'Nominal','Synthetic','Expected','Term Premium','CIP Dev'};
-input.dataFormat = {'%.2f'};
-input.fontSize = 'tiny';
-namefl   = fullfile('..','..','Docs','Tables','temp_decomp10yr');
-input.data = AvgDecomp;
-input.tableCaption = '10-Year Yield Decomposition (\%).';
-input.tableLabel = 'decomp10yr';
-input.texName = namefl;
-latexTable(input);
-
-
-AvgTPnomvssyn = [mean(tpcompare(1:15,:)); mean(tpcompare([16:19 23:25],:)); mean(tpcompare(20:22,:))];
-clear input
-labelcty    = {'EM','A-SOE','G-3'};
-labelcty{1} = [' ' labelcty{1}];                % Otherwise, Latex gives an error
-input.tableRowLabels = labelcty;
-input.tableColLabels = {'Nominal','Synthetic'};
-input.dataFormat = {'%.2f'};
-input.fontSize = 'tiny';
-namefl   = fullfile('..','..','Docs','Tables','temp_tp_compare');
-input.data = AvgTPnomvssyn;
-input.tableCaption = '10-Year Term Premium Comparison (\%).';
-input.tableLabel = 'tp_compare10yr';
-input.texName = namefl;
-latexTable(input);
-
-
-%%
-
-
-% %% Term Spread
+% %% Decomposition of Nominal Yield Curves and TP Comparison
 % 
 % ncntrs  = length(S);
-% Mlong  = 10;
-% Mshort = 0.25; %2;
-% tsnomvssyn = nan(ncntrs,3);
-% tsmean = nan(ncntrs,3);
-% corrTSprd = nan(ncntrs,2);
+% tnr = 10;
+% decomp = nan(ncntrs,5);
+% tpvslccs = nan(ncntrs,3);
+% tpcompare = nan(ncntrs,2);
+% tpnomvssyn = nan(ncntrs,3);
 % 
 % for k = 1:ncntrs
-%     x = S(k).nomtsprd(:,2);
-%     y = S(k).syntsprd(:,2);
-%     [h,p] = ttest2(x,y);     % Equality of means test b/w TSnom and TSsyn
-%     tsnomvssyn(k,:) = [k h p]; % Null of equality is not rejected for any country
-%     % The mean of the 10yr-3m spread is higher than the mean of the 10-2yr spread
-%     % corr of 10-2yr spread b/w nom and syn is generally higher than corr of 10yr-3m spread
+%     yieldsnom = mean(S(k).nomblncd(2:end, S(k).nomblncd(1,:) == tnr)*100);
+%     yieldssyn = mean(S(k).synblncd(2:end, S(k).synblncd(1,:) == tnr)*100);
+%     yieldsP   = mean(S(k).synyldsP(2:end, S(k).synyldsP(1,:) == tnr)*100);
+%     x = S(k).syntp(3:end, S(k).syntp(1,:) == tnr);
+%     tpsyn     = mean(x);
+%     y = S(k).cipdev(2:end, S(k).cipdev(1,:) == tnr);
+%     lccs      = mean(y,'omitnan');
+%     decomp(k,:) = [yieldsnom yieldssyn yieldsP tpsyn lccs];
+%     [h,p] = ttest2(x,y,'Vartype','unequal');     % Equality of means test b/w TP and LCCS
+%     tpvslccs(k,:) = [k h p];
+%     
+%     z = S(k).nomtp(3:end, S(k).nomtp(1,:) == tnr);
+%     tpnom     = mean(z);
+%     tpcompare(k,:) = [tpnom tpsyn];
+%     [h,p] = ttest2(z,x,'Vartype','unequal');     % Equality of means test b/w TP and LCCS
+%     tpnomvssyn(k,:) = [k h p];
 % end
-
-%% Average over a field: EM vs AE and Nom vs Syn
-
-fname = 'RMSEb'; % 'PCb'
-fnom = ['nom' fname];
-fsyn = ['syn' fname];
-AvgEM_AE = nan(2,2);
-% AvgEM_AE = cell(3,3);
-% AvgEM_AE{1,2} = 'Nominal';  AvgEM_AE{1,3} = 'Synthetic';
-% AvgEM_AE{2,1} = 'EM';       AvgEM_AE{3,1} = 'AE';
-
-AvgNomb = 0; AvgSynb = 0;
-for k = 1:15
-    AvgNomb = AvgNomb + S(k).(fnom);
-    AvgSynb = AvgSynb + S(k).(fsyn);
-end
-AvgEMnom = AvgNomb/15; AvgEMsyn = AvgSynb/15;
-
-AvgNomb = 0; AvgSynb = 0;
-for k = 16:25
-    AvgNomb = AvgNomb + S(k).(fnom);
-    AvgSynb = AvgSynb + S(k).(fsyn);
-end
-AvgAEnom = AvgNomb/10; AvgAEsyn = AvgSynb/10;
-clear AvgNomb AvgSynb
-
-% AvgEM_AE{2,2} = AvgEMnom;  AvgEM_AE{2,3} = AvgEMsyn;
-% AvgEM_AE{3,2} = AvgAEnom;  AvgEM_AE{3,3} = AvgAEsyn;
-AvgEM_AE(1,1) = AvgEMnom;  AvgEM_AE(1,2) = AvgEMsyn;
-AvgEM_AE(2,1) = AvgAEnom;  AvgEM_AE(2,2) = AvgAEsyn;
-
-clear input
-labelcty    = {'EM','AE'};
-labelcty{1} = [' ' labelcty{1}];                % Otherwise, Latex gives an error
-input.tableRowLabels = labelcty;
-input.tableColLabels = {'Nominal','Synthetic'};
-input.dataFormat = {'%.2f'};
-input.fontSize = 'tiny';
-namefl   = fullfile('..','..','Docs','Tables','temp_RMSE_ATSM');
-input.data = AvgEM_AE;
-input.tableCaption = 'Fit of Affine Term Structure Models.';
-input.tableLabel = 'rmse_atsm';
-input.texName = namefl;
-latexTable(input);
-
+% 
+% AvgDecomp = [mean(decomp(1:15,:)); mean(decomp([16:19 23:25],:)); mean(decomp(20:22,:))];
+% clear input
+% labelcty    = {'EM','A-SOE','G-3'};
+% labelcty{1} = [' ' labelcty{1}];                % Otherwise, Latex gives an error
+% input.tableRowLabels = labelcty;
+% input.tableColLabels = {'Nominal','Synthetic','Expected','Term Premium','CIP Dev'};
+% input.dataFormat = {'%.2f'};
+% input.fontSize = 'tiny';
+% namefl   = fullfile('..','..','Docs','Tables','temp_decomp10yr');
+% input.data = AvgDecomp;
+% input.tableCaption = '10-Year Yield Decomposition (\%).';
+% input.tableLabel = 'decomp10yr';
+% input.texName = namefl;
+% latexTable(input);
