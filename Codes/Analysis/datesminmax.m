c4047e36356023be0a4c4bc857c2b79f8482d102
Pavel Solís <pavel.solis@gmail.com>
2020-08-02 10:27:47 -0400

    Update prefix of field name for dateb (from n_ and s_ to mn_ and ms_)

--- Codes/Analysis/datesminmax.m
+++ Codes/Analysis/datesminmax.m
@@ -4,7 +4,7 @@ function [dtmn,dtmx] = datesminmax(S,k0)
 
 % Pavel Solís (pavel.solis@gmail.com), June 2020
 %%
-date1 = datenum(S(k0).n_dateb,'mmm-yyyy');
-date2 = datenum(S(k0).s_dateb,'mmm-yyyy');
+date1 = datenum(S(k0).mn_dateb,'mmm-yyyy');
+date2 = datenum(S(k0).ms_dateb,'mmm-yyyy');
 dtmn  = min(date1,date2); 
 dtmx  = max(date1,date2);
