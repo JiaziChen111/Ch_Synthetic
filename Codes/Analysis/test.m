3588b68c98e5b000321a769f7909b9cc3be17701
Pavel Solís <pavel.solis@gmail.com>
2020-08-02 15:35:09 -0400

    Clean

--- Codes/Analysis/test.m
+++
@@ -1,9 +0,0 @@
-usycnms = TTusyc.Properties.VariableNames;
-fltrMTH = contains(usycnms,'M');
-tenors  = cell2mat(cellfun(@str2double,regexp(usycnms,'\d*','Match'),'UniformOutput',false));
-tenors  = [tenors(fltrMTH),tenors(~fltrMTH)*12];
-fltrGSW = ismember(tenors,matsout*12);
-TTgsw   = TTusyc(:,fltrGSW);
-TTgsw.Properties.VariableNames = strcat('usyc',tenors(fltrGSW),'m');
-
-TT_kw = array2timetable(TT_kw{:,:}/100,'RowTimes',TT_kw.Time,'VariableNames',TT_kw.Properties.VariableNames);
