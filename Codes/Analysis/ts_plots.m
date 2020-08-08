d28965334151e4ffd1f26956858d35938c092821
Pavel Solís <pavel.solis@gmail.com>
2020-08-08 10:05:53 -0400

    Update y limits in figures for survey forecasts

--- Codes/Analysis/ts_plots.m
+++ Codes/Analysis/ts_plots.m
@@ -103,13 +103,16 @@ for k0 = 1:length(macrovr)
             plot(S(k1).(fldname)(fltrd,1),S(k1).(fldname)(fltrd,end),...
                 S(k1).(fldname)(fltrd,1),S(k1).(fldname)(fltrd,end-1),'-.');
             title(S(k1).cty);
-            if k1 == 12; legend({'10Y','5Y'},'Location','southwest','AutoUpdate','off'); end
+            if k1 == 13; legend({'10Y','5Y'},'Location','southwest','AutoUpdate','off'); end
             datetick('x','yy');
             L = get(gca,'XLim'); set(gca,'XTick',linspace(L(1),L(2),4))     % sets #ticks to 4
-            [ld,lu] = inflation_target(S(k1).iso);
-            if ~isempty(ld); yline(ld,'--'); yline(lu,'--'); end
-            ylim([0 8]); % yline(0);
-            
+            if strcmp(macrovr{k0},'CPI')
+                [ld,lu] = inflation_target(S(k1).iso);
+                if ~isempty(ld); yline(ld,'--'); yline(lu,'--'); end
+                ylim([0 8]);
+            elseif strcmp(macrovr{k0},'GDP');   ylim([0 7.5]);
+            else;  ylim([0 8]); % yline(0);
+            end
         end
     end
     figname = ['wn' macrovr{k0}]; save_figure(figdir,figname,formats,figsave)
