b1d3fd8f69c41abcc64da740168e2e2913c57db2
Pavel Solís <pavel.solis@gmail.com>
2020-09-05 15:26:19 -0400

    Add call to atsm_estimation w/ option for fminunc

--- Codes/Analysis/ts_analysis.m
+++ Codes/Analysis/ts_analysis.m
@@ -20,7 +20,7 @@ S = daily2dymy(S,dataset_daily,header_daily,true);
 S = add_macroNsvys(S,currEM);
 S = append_svys2ylds(S,currEM);
 
-%% Estimate affine term structure model
+%% Estimate affine term structure model (fminsearch for all cases)
 matsout = [0.25 0.5 1 2 5 10];                                      % report 3M-6M-1Y-2Y-5Y-10Y tenors
 datetime(now(),'ConvertFrom','datenum')
 S = atsm_estimation(S,matsout,false);                               % fixed sgmS case, runtime 4.1 hrs
@@ -30,13 +30,13 @@ datetime(now(),'ConvertFrom','datenum')
 
 %% Baseline estimations
 ncntrs  = length(S);
-fldname = {'mssb_','mny_'};  % fldname = {'mssb_','msy_','mny_'};
+fldname = {'mssb_','mny_'};                                         % EM and AE
 fldtype = {'yQ','yP','tp','pr'};
 ntypes  = length(fldtype);
 for k0  = 1:ncntrs
     for k1 = 1:ntypes
         if ismember(S(k0).iso,currEM)
-            fldaux = fldname{1};                                    % synthetic yields, surveys,fixed sgmS
+            fldaux = fldname{1};                                    % synthetic yields + surveys + fixed sgmS
         else
             fldaux = fldname{2};                                    % nominal yields
         end
@@ -44,9 +44,14 @@ for k0  = 1:ncntrs
     end
 end
 
-% Assess fit of the model
-[S,fitrprtmy] = assess_fit(S,currEM,currAE,false);
+%% Estimate affine term structure model (fminunc for baseline cases)
+matsout = [0.25 0.5 1 2 5 10];                                      % report 3M-6M-1Y-2Y-5Y-10Y tenors
+datetime(now(),'ConvertFrom','datenum')
+S = atsm_estimation(S,matsout,false,false);                      	% fixed sgmS case, runtime 4.1 hrs
+datetime(now(),'ConvertFrom','datenum')
 
+%% Assess fit of the model for baseline estimation
+[S,fitrprtmy] = assess_fit(S,currEM,currAE,false);
 S = add_vars(S,currEM);
 
 %% Daily frequency estimation
