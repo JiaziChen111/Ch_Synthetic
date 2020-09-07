31adc63647c0b71b83e9fd97f84fe3037e9ee054
Pavel Solís <pavel.solis@gmail.com>
2020-09-07 14:32:25 -0400

    Include computation of SE for credit risk

--- Codes/Analysis/se_components.m
+++ Codes/Analysis/se_components.m
@@ -2,7 +2,7 @@ function S = se_components(S,matsout,currEM)
 % SE_COMPONENTS Report standard errors for yield components due to uncertainty 
 % in the estimated parameters. The state is assumed to be known with certainty
 
-% m-files called: vars2parest, parest2vars, loadings
+% m-files called: syncdatasets, vars2parest, parest2vars, loadings
 % Pavel Solís (pavel.solis@gmail.com), September 2020
 %%
 dt      = 1/12;
@@ -13,16 +13,15 @@ fnames  = fieldnames(S);
 fnameq  = fnames{contains(fnames,'bsl_pr')};                                % field containing estimated parameters
 
 for k0  = 1:nEMs
-%     % Nominal yields
-%     fnameb = 'mn_blncd';                                                    % field containing *nominal* yields
-%     fltrnm = ismember(S(k0).(fnameb)(1,:),matsout);
-%     yldnom = S(k0).(fnameb)(2:end,fltrnm);                                  % yields in decimals
-%     nobsnm = size(yldnom,1);                                                % number of observations
-%     datesn = S(k0).(fnameb)(2:end,1);                                       % dates
+    % Nominal yields
+    fnameb = 'mn_blncd';                                                    % field containing *nominal* yields
+    fltrnm = ismember(S(k0).(fnameb)(1,:),matsout);                         % same maturities as in matsout
+    yldnom = S(k0).(fnameb)(2:end,fltrnm);                                  % yields in decimals
+    datesn = S(k0).(fnameb)(2:end,1);                                       % dates
     
     % Synthetic yields
     fnameb = 'ms_blncd';                                                    % field containing *synthetic* yields
-    fltrsn = ismember(S(k0).(fnameb)(1,:),matsout);
+    fltrsn = ismember(S(k0).(fnameb)(1,:),matsout);                         % same maturities as in matsout
     yldsyn = S(k0).(fnameb)(2:end,fltrsn);                                  % yields in decimals
     nobssn = size(yldsyn,1);                                                % number of observations
     datess = S(k0).(fnameb)(2:end,1);                                       % dates
@@ -41,8 +40,11 @@ for k0  = 1:nEMs
     [AnP,BnP] = loadings(matsout,mu_xP,PhiP,Hcov,rho0,rho1,dt);
     yQold     = ones(nobssn,1)*AnQ + xs*BnQ;
     yPold     = ones(nobssn,1)*AnP + xs*BnP;
-    tpold     = yQold  - yPold;
-%     phiold    = yldnom - yQold;
+    tpold     = yQold - yPold;
+    [~,crynom,cryQold] = syncdatasets([nan matsout; datesn yldnom],[nan matsout; datess yQold]);
+    crold     = crynom(2:end,2:end) - cryQold(2:end,2:end);
+    datesc    = crynom(2:end,1);
+    nobscr    = length(datesc);
     
     % Delta method
     thetaold = vars2parest(PhiP,cSgm,lmbd1,lmbd0,mu_xP,rho1,rho0,sgmY,sgmS);
@@ -50,7 +52,7 @@ for k0  = 1:nEMs
     JyQ      = nan(nmats,ntheta,nobssn);
     JyP      = nan(nmats,ntheta,nobssn);
     Jtp      = nan(nmats,ntheta,nobssn);
-%     Jphi    = nan(nmats,ntheta,nobssn);                                      % check: nobsnm or nobssn
+    Jcr      = nan(nmats,ntheta,nobscr);
     for k1 = 1:ntheta
         % Subtract epsilon to theta
         thetanew     = thetaold;
@@ -64,35 +66,36 @@ for k0  = 1:nEMs
         [AnP,BnP] = loadings(matsout,mu_xP,PhiP,Hcov,rho0,rho1,dt);
         yQnew     = ones(nobssn,1)*AnQ + xs*BnQ;
         yPnew     = ones(nobssn,1)*AnP + xs*BnP;
-        tpnew     = yQnew  - yPnew;
-%         phinew    = yldnom - yQnew;
+        tpnew     = yQnew - yPnew;
+        [~,crynom,cryQnew] = syncdatasets([nan matsout; datesn yldnom],[nan matsout; datess yQnew]);
+        crnew     = crynom(2:end,2:end) - cryQnew(2:end,2:end);
 
         % Jacobians
-        JyQ(:,k1,:)  = (yQold - yQnew)'/epsilon;
-        JyP(:,k1,:)  = (yPold - yPnew)'/epsilon;
-        Jtp(:,k1,:)  = (tpold - tpnew)'/epsilon;
-%         Jphi(:,k1,:) = (phiold - phinew)/epsilon;
+        JyQ(:,k1,:) = (yQold - yQnew)'/epsilon;
+        JyP(:,k1,:) = (yPold - yPnew)'/epsilon;
+        Jtp(:,k1,:) = (tpold - tpnew)'/epsilon;
+        Jcr(:,k1,:) = (crold - crnew)'/epsilon;
     end
     
     % Standard errors
-    seyQ  = nan(nobssn,nmats);
-    seyP  = nan(nobssn,nmats);
-    setp  = nan(nobssn,nmats);
-%     sephi = nan(nobssn,nmats);
+    seyQ = nan(nobssn,nmats);
+    seyP = nan(nobssn,nmats);
+    setp = nan(nobssn,nmats);
+    secr = nan(nobscr,nmats);
     for k2 = 1:nobssn
-        seyQ(k2,:)  = sqrt(diag(JyQ(:,:,k2)*(Hhess\JyQ(:,:,k2)')));
-        seyP(k2,:)  = sqrt(diag(JyP(:,:,k2)*(Hhess\JyP(:,:,k2)')));
-        setp(k2,:)  = sqrt(diag(Jtp(:,:,k2)*(Hhess\Jtp(:,:,k2)')));
-%         sephi(k2,:) = sqrt(diag(Jphi(:,:,k2)*Hhess\Jphi(:,:,k2)'));
+        seyQ(k2,:) = sqrt(diag(JyQ(:,:,k2)*(Hhess\JyQ(:,:,k2)')));
+        seyP(k2,:) = sqrt(diag(JyP(:,:,k2)*(Hhess\JyP(:,:,k2)')));
+        setp(k2,:) = sqrt(diag(Jtp(:,:,k2)*(Hhess\Jtp(:,:,k2)')));
+    end
+    for k2 = 1:nobscr
+        secr(k2,:) = sqrt(diag(Jcr(:,:,k2)*(Hhess\Jcr(:,:,k2)')));
     end
     
-    S(k0).('bsl_yQ_se')  = [nan matsout; datess seyQ];
-    S(k0).('bsl_yP_se')  = [nan matsout; datess seyP];
-    S(k0).('bsl_tp_se')  = [nan matsout; datess setp];
-%     S(k0).('bsl_phi_se') = [nan matsout; datess sephi];
+    S(k0).('bsl_yQ_se') = [nan matsout; datess seyQ];
+    S(k0).('bsl_yP_se') = [nan matsout; datess seyP];
+    S(k0).('bsl_tp_se') = [nan matsout; datess setp];
+    S(k0).('bsl_cr_se') = [nan matsout; datesc secr];
 end
-
-% correct phi b/c yldnom and yldsyn are not synchronized
 
 %     % Covariance matrix of state vector based on estimated parameters
 %     x00   = S(k0).(fname0).x00;     P00   = S(k0).(fname0).P00;
