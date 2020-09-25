a72ee8bbe134fc9240bafa467c46943023112c34
Pavel Solís <pavel.solis@gmail.com>
2020-09-25 11:17:21 -0400

    Include legends and save option for figures

--- Codes/Analysis/compare_spf_tips.m
+++ Codes/Analysis/compare_spf_tips.m
@@ -12,12 +12,20 @@ rr5y  = rmmissing(rr5y);
 rr10y = rmmissing(rr10y);
 
 %% Figures
+figdir  = 'Estimation'; formats = {'eps'}; figsave = true;
+
 figure
 plot(rr1y.Time, [rr1y.(1)  rr1y.(2)])
+legend({'SPF','TIPS'})
+figname = 'spf_tips_1y'; save_figure(figdir,figname,formats,figsave)
 figure
 plot(rr5y.Time, [rr5y.(1)  rr5y.(2)])
+legend({'SPF','TIPS'})
+figname = 'spf_tips_5y'; save_figure(figdir,figname,formats,figsave)
 figure
 plot(rr10y.Time,[rr10y.(1) rr10y.(2)])
+legend({'SPF','TIPS'})
+figname = 'spf_tips_10y'; save_figure(figdir,figname,formats,figsave)
 
 %% Summary statistics
 % 1Y
