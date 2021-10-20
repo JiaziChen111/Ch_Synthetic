7cb66cb5ff753d52149037dc12d6cf64d8286da0
Pavel Solís <pavel.solis@gmail.com>
2021-10-20 13:40:05 -0500

    Reorder and comment lines (free sgmS, fminunc), uncomment save lines so that runs all at once

--- Codes/Analysis/ts_analysis.m
+++ Codes/Analysis/ts_analysis.m
@@ -4,7 +4,7 @@
 % m-files called: daily2dymy, add_macroNsvys, append_svys2ylds, atsm_estimation, se_state, add_cr, 
 % se_components, assess_fit, add_vars, ts_plots, ts_correlations, ts_pca, atsm_daily, construct_panel
 % auxiliary: read_macrovars, read_kw
-% Pavel Solís (pavel.solis@gmail.com), August 2021
+% Pavel Solís (pavel.solis@gmail.com), October 2021
 % 
 %% Load the data
 clear
@@ -21,17 +21,28 @@ S = daily2dymy(S,dataset_daily,header_daily,true);
 S = add_macroNsvys(S,currEM);
 S = append_svys2ylds(S,currEM);
 
-%% Estimate affine term structure model using fminsearch (for all cases)
+%% Estimate affine term structure model using fminsearch (fixed and free sgmS cases)
 matsout = [0.25 0.5 1 2 5 10];                                      % report 3M-6M-1Y-2Y-5Y-10Y tenors
+
+% Fixed sgmS case, runtime 5.5 hrs
 datetime(now(),'ConvertFrom','datenum')
-S = atsm_estimation(S,matsout,false);                               % fixed sgmS case, runtime 5.5 hrs
+S = atsm_estimation(S,matsout,false);
 datetime(now(),'ConvertFrom','datenum')
-S = atsm_estimation(S,matsout,true);                                % free sgmS case, runtime 4.5 hrs
-datetime(now(),'ConvertFrom','datenum')
+
+% % Free sgmS case, runtime 4.5 hrs
+% % Not needed, but if done, better by country; if so, do it at the end and use: save('struct_datamy_S.mat','S','-append')
+% S = atsm_estimation(S,matsout,true);
+% datetime(now(),'ConvertFrom','datenum')
+
+%% Estimate affine term structure model using fminunc (fixed sgmS case)
+% sgmSfree = false;                                                   % consistent w/ baseline (fixed sgmS) case
+% datetime(now(),'ConvertFrom','datenum')
+% S = atsm_estimation(S,matsout,sgmSfree,false);                      % fminunc, runtime ~20 min
+% datetime(now(),'ConvertFrom','datenum')
 
 %% Baseline estimations
 ncntrs  = length(S);
-fldname = {'mssb_','mny_'};                                         % EM and AE
+fldname = {'mssb_','mny_'};                                             % EM and AE
 fldtype = {'yQ','yP','tp','pr'};
 ntypes  = length(fldtype);
 for k0  = 1:ncntrs
@@ -44,12 +55,6 @@ for k0  = 1:ncntrs
         S(k0).(['bsl_' fldtype{k1}]) = S(k0).([fldaux fldtype{k1}]);
     end
 end
-
-%% Estimate affine term structure model using fminunc (for baseline cases)
-sgmSfree = false;                                                   % consistent w/ baseline (fixed sgmS) case
-datetime(now(),'ConvertFrom','datenum')
-S = atsm_estimation(S,matsout,sgmSfree,false);                      % fminunc, runtime ~20 min
-datetime(now(),'ConvertFrom','datenum')
 
 %% Post-estimation computations
 seX = se_state(S,currEM);
@@ -65,9 +70,9 @@ S = daily2dymy(S,dataset_daily,header_daily,false);
 
 %% Store/load results
 cd(pathd)
-% save struct_datamy_S.mat S currAE currEM fitrprtdy fitrprtmy
-load('struct_datamy_S.mat')
-load('struct_datady_cells.mat')
+save struct_datamy_S.mat S currAE currEM fitrprtdy fitrprtmy
+% load('struct_datamy_S.mat')
+% load('struct_datady_cells.mat')
 cd(pathc)
 
 %% Post-estimation analysis
@@ -78,10 +83,11 @@ vix = data_macro(:,ismember(hdr_macro(:,2),{'type','VIX'}));
 ts_plots(S,currEM,currAE,kwtp,vix,true);
 [corrTPem,corrTPae,corrBRP,corrTPyP] = ts_correlations(S,currEM,currAE,kwtp,vix);
 [pcexplnd,pc1em,pc1ae,pc1res,r2TPyP] = ts_pca(S,currEM,currAE,kwyp,kwtp);
-% cd(pathd)
-% save struct_datamy_S.mat S currAE currEM fitrprtdy fitrprtmy corrTPem corrTPae ...
-% corrTPyP pcexplnd pc1em pc1ae pc1res r2TPyP
-% cd(pathc)
+
+cd(pathd)
+save struct_datamy_S.mat S currAE currEM fitrprtdy fitrprtmy corrTPem corrTPae ...
+corrTPyP pcexplnd pc1em pc1ae pc1res r2TPyP
+cd(pathc)
 
 %% Construct panel dataset
 datetime(now(),'ConvertFrom','datenum')
